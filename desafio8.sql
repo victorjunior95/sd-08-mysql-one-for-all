9adceb5096c110e041137a745f58b863c5882f79
Bruno Wedley <brunowedley27@gmail.com>
2021-04-27 17:44:41 -0300

    adiciona desafios 8 e 9

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+	BEFORE DELETE ON SpotifyClone.Users
+    FOR EACH ROW
+    BEGIN
+    DELETE FROM SpotifyClone.Playback_history
+    WHERE OLD.user_id = user_id;
+    DELETE FROM SpotifyClone.Artist_followers
+    WHERE OLD.user_id = user_id;
+    END $$
+DELIMITER ;
+
---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artistname VARCHAR(50))
+BEGIN
+SELECT a.artist AS artista,
+al.album AS album
+FROM SpotifyClone.Artists AS a
+INNER JOIN SpotifyClone.Albums AS al ON a.artist_id = al.artist_id
+WHERE artistname = a.artist
+ORDER BY al.album;
+END $$
+
+DELIMITER ;
+
+
+CALL albuns_do_artista('Walter Phoenix');
