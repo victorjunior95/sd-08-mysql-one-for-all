e2366dee3b7e373f083c32a1bbf911aa80c4b6a8
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-04-27 19:05:59 -0300

    corrige 9

--- desafio9.sql
+++ desafio9.sql
@@ -4,12 +4,13 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(artista_nome VARCHAR(50))
 BEGIN
   SELECT 
-  artista, album
+  artista AS `artista`,
+  album AS `album`
   FROM SpotifyClone.artistas AS art
   INNER JOIN SpotifyClone.albuns AS alb
   ON art.artista_id = alb.artista_id
   WHERE artista = artista_nome
-  ORDER BY album;
+  ORDER BY `album`;
 END $$
 
 DELIMITER ;
