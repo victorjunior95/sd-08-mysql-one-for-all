98db8f572557130697ba2e785737feb0d9de1e8d
Clênio Galvão <clenio.cgm@gmail.com>
2021-04-27 17:57:42 -0300

    Resolve desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+CREATE VIEW historico_reproducao_usuarios AS 
+SELECT u.usuario_nome AS usuario, m.musica_nome FROM SpotifyClone.usuarios u
+INNER JOIN SpotifyClone.reproducoes r
+ON r.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.musicas m
+ON r.musica_id = m.musica_id
+ORDER BY 1, 2;
