67a5acfbf1484433c6fe45705dda3fd91ece5837
liisaambrosen <liisa.ambrosen@gmail.com>
2021-04-27 18:45:58 -0300

    erro

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,7 @@
-USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(song_id) AS cancoes,
 COUNT(DISTINCT(a.artist_id)) AS artistas,
 COUNT(DISTINCT(a.album_id)) AS albuns
-FROM Songs AS s
-INNER JOIN Albums as a
+FROM SpotifyClone.Songs AS s
+INNER JOIN SpotifyClone.Albums as a
 ON a.album_id = s.album_id;
