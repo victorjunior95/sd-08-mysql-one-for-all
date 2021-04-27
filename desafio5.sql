45af45fcb588ee840846aa2c000afa8e59193a8d
emassaki <massakihirayama@gmail.com>
2021-04-27 17:31:06 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.nome AS cancao, COUNT(*) AS reproducoes
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico AS h ON c.cancao_id = h.cancao_id
+GROUP BY h.cancao_id
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
