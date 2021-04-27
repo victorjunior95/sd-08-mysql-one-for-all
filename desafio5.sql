d286efdc4215a9c1e92c90985f908ee09930b825
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-04-27 17:57:51 -0300

    desafios 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW `top_2_hits_do_momento` AS
+SELECT `song_name` AS `cancao`,
+COUNT(`user_id`) AS `reproducoes`
+FROM `SpotifyClone`.`user_history` AS H
+INNER JOIN `SpotifyClone`.`songs` AS S ON S.`song_id` = H.`song_id`
+GROUP BY H.`song_id`
+ORDER BY `reproducoes` DESC, `cancao`
+LIMIT 2;
