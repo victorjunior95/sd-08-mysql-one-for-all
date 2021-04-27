c4582e6d66b15aaa4d8828bc7515171b83893da1
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-27 19:13:51 -0300

    Challenge-6-done

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+  ROUND(MIN(P.price), 2) AS faturamento_minimo,
+  ROUND(MAX(P.price), 2) AS faturamento_maximo,
+  ROUND(AVG(P.price), 2) AS faturamento_medio,
+  ROUND(SUM(P.price), 2) AS faturamento_total
+FROM SpotifyClone.plans AS P
+INNER JOIN SpotifyClone.users AS U
+  ON P.plan_id = U.plan_id;
