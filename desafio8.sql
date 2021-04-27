28e036d33f49f23cc151ba2ebefd63bd78d6e247
Clênio Galvão <clenio.cgm@gmail.com>
2021-04-27 19:00:59 -0300

    Resolve desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.seguindo
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.reproducoes
+WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
