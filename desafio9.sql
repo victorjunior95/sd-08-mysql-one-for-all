026a6c3d1659288c46322186e1c809f319dc76db
Fabio Ferreira <fabio35547@gmail.com>
2021-04-27 18:59:16 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(100))
+BEGIN
+SELECT 
+	A.nome AS 'artista',
+	B.nome AS 'album'
+FROM 
+	SpotifyClone.artista A
+		JOIN 
+	SpotifyClone.album B ON A.artista_id = B.artista_id
+WHERE 
+	A.nome = nome_artista
+ORDER BY B.nome;
+END
+
+$$ DELIMITER ;
