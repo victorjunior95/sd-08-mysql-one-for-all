73f0b54744e10f35a900882fb89071532b33e4c6
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-04-27 20:26:54 -0300

    fix: desafio 4 e 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_2_hits_do_momento AS
-    c.titulo AS cancoes,
   SELECT
+    c.titulo AS cancoes,
     COUNT(*) AS reproducoes
   FROM SpotifyClone.cancoes AS c
   INNER JOIN SpotifyClone.reproducoes AS r
