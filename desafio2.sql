b1dddafd06c8576ada35d63734fe1d060a3ab328
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-27 15:57:32 -0300

    adicionando desafio2

---
+++ desafio2.sql
@@ -0,0 +1,3 @@
+USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS SELECT * FROM (SELECT COUNT(cancao_id) AS `cancoes` FROM Cancoes) AS `cancoes`,
+(SELECT COUNT(artista_id) AS `artistas` FROM Artistas) AS `artistas`, ( SELECT COUNT(album_id) AS `albuns` FROM Albuns) AS `albuns`;
