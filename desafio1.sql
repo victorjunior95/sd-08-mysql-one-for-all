fe3cf54ffad3aad96b0e6a22d6605c8f49fd59da
Tandy <tandyfw@gmail.com>
2021-04-27 19:12:14 -0300

    Desafio 1-hotfix-3

--- desafio1.sql
+++ desafio1.sql
@@ -67,7 +67,7 @@ INSERT INTO cancao (cancao, album_id) VALUES
 ("Soul For Us", 1), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2),
 ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3), ("Fantasy For Me", 4), ("Celebration Of More", 4),
 ("Rock His Everything", 4), ("Home Forever", 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5),
-("Words Of Her Life", 5), ("Words Of Her Life", 5);
+("Words Of Her Life", 5), ("Without My Streets", 5);
 
 INSERT INTO historico_de_reproducoes (usuario_id, cancao_id) VALUES (1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2, 2), (2, 15),
 (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
