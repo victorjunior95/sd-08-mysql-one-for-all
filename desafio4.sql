055f428b62ef024b9f2650e13a47baf2c2ea3dd7
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-04-27 17:07:25 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+artistas.artista AS "artista",
+COUNT(artista) AS"seguidores"
+FROM SpotifyClone.artistas AS artistas
+INNER JOIN SpotifyClone.artistas_seguidos AS seguidos
+ON artistas.artista_id = seguidos.artista_id
+GROUP BY artista
+ORDER BY `seguidores` DESC
+LIMIT 3;
