4ca3d7056c3cd797341c1f27fdb58b63d705b68c
Tandy <tandyfw@gmail.com>
2021-04-27 17:44:00 -0300

    Desafio 4 concluido

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+
+CREATE VIEW top_3_artistas AS
+SELECT artista, COUNT(*) AS seguidores
+FROM SpotifyClone.seguindo_artistas AS sa
+INNER JOIN SpotifyClone.artista AS a
+ON a.artista_id = sa.artista_id
+GROUP BY sa.artista_id
+ORDER BY seguidores DESC, artista
+LIMIT 3;
