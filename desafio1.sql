5083e0aca745229196b006a2f537f5c6e63ddeb9
Igor <igorln96@gmail.com>
2021-04-27 19:50:30 -0300

    fix

--- desafio1.sql
+++ desafio1.sql
@@ -74,7 +74,7 @@ INSERT INTO Cancoes (titulo, album_id) VALUES
 ("Soul For Us", 1), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2),
 ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3), ("Fantasy For Me", 4), ("Celebration Of More", 4),
 ("Rock His Everything", 4), ("Home Forever", 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5),
-("Words Of Her Life", 5), ("Words Of Her Life", 5);
+("Words Of Her Life", 5), ("Without My Streets", 5);
 
 INSERT INTO Seguindo (user_id, artista_id) VALUES (1, 1), (1, 4), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1), (4, 4);
 
