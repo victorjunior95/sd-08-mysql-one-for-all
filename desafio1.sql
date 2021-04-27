6c2facdeb12b8948041becb01d91ee46855bc0b0
Leandro Lachini <leandrolachini@gmail.com>
2021-04-27 20:10:38 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -13,19 +13,6 @@ VALUES
   ('gratuito', 0),
   ('familiar', 7.99),
   ('universitário', 5.99);
-
-CREATE TABLE album(
-    album_id INT PRIMARY KEY auto_increment,
-    album VARCHAR(30) NOT NULL
-) engine = InnoDB;
-
-INSERT INTO album (album)
-VALUES
-  ('Envious'),
-  ('Exuberant'),
-  ('Hallowed Steam'),
-  ('Incandescent'),
-  ('Temporary Culture');
   
 CREATE TABLE usuario(
     usuario_id INT PRIMARY KEY auto_increment,
@@ -53,32 +40,49 @@ VALUES
   ('Peter Strong'),
   ('Lance Day'),
   ('Freedie Shannon');
+  
+CREATE TABLE album(
+    album_id INT PRIMARY KEY auto_increment,
+    album VARCHAR(30) NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
+) engine = InnoDB;
+
+INSERT INTO album (album, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
 
 CREATE TABLE cancoes(
     cancao_id INT PRIMARY KEY auto_increment,
-    nome_cancao VARCHAR(40) NOT NULL
+    nome_cancao VARCHAR(40) NOT NULL,
+	album_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES album (album_id)
 ) engine = InnoDB;
 
-INSERT INTO cancoes (nome_cancao)
+INSERT INTO cancoes (nome_cancao, album_id)
 VALUES
-  ("Soul For Us"),
-  ("Reflections Of Magic"),
-  ("Dance With Her Own"),
-  ("Troubles Of My Inner Fire"),
-  ("Time Fireworks"),
-  ("Magic Circus"),
-  ("Honey, So Do I"),
-  ("Sweetie, Let's Go Wild"),
-  ("She Knows"),
-  ("Fantasy For Me"),
-  ("Celebration Of More"),
-  ("Rock His Everything"),
-  ("Home Forever"),
-  ("Diamond Power"),
-  ("Honey, Let's Be Silly"),
-  ("Thang Of Thunder"),
-  ("Words Of Her Life"),
-  ("Without My Streets"); 
+  ("Soul For Us", 1),
+  ("Reflections Of Magic", 1),
+  ("Dance With Her Own", 1),
+  ("Troubles Of My Inner Fire", 2),
+  ("Time Fireworks", 2),
+  ("Magic Circus", 3),
+  ("Honey, So Do I", 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ("She Knows", 3),
+  ("Fantasy For Me", 4),
+  ("Celebration Of More", 4),
+  ("Rock His Everything", 4),
+  ("Home Forever", 4),
+  ("Diamond Power", 4),
+  ("Honey, Let's Be Silly", 4),
+  ("Thang Of Thunder", 5),
+  ("Words Of Her Life", 5),
+  ("Without My Streets", 5); 
 
 CREATE TABLE historico_de_reproducoes(
     usuario_id INT,
