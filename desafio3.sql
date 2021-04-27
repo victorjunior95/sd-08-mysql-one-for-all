9411be5bfda71e3fd534a653241fd0d038b12d55
Bruno Wedley <brunowedley27@gmail.com>
2021-04-27 16:19:15 -0300

    teste em desafio 3 e adiciona desafio 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 USE SpotifyClone;
 
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.username AS usuario, s.song AS nome FROM Users AS u
-INNER JOIN Playback_history AS p ON p.user_id = u.user_id
-INNER JOIN Songs AS s ON s.song_id = p.song_id
+SELECT u.username AS usuario, s.song AS nome FROM SpotifyClone.Users AS u
+INNER JOIN SpotifyClone.Playback_history AS p ON p.user_id = u.user_id
+INNER JOIN SpotifyClone.Songs AS s ON s.song_id = p.song_id
 ORDER BY usuario, nome;
---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+USE SpotifyClone;
+
+CREATE VIEW top_3_artistas AS
+SELECT a.artist AS artista, COUNT(f.artist_id) AS seguidores
+FROM Artists AS a
+INNER JOIN Artist_followers AS f ON f.artist_id = a.artist_id 
+GROUP BY artista
+ORDER BY seguidores DESC, artista
