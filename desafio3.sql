5e2edbc1b69983eba2d4576bee63a2eaf4e88fc1
albertassi88 <albertassi88@gmail.com>
2021-04-27 20:12:33 -0300

    Criacao do desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+SELECT u.nome_usuario AS 'usuario', c.nome_cancao AS 'nome'
+FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.historico_reproducoes AS h
+ON h.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.cancoes AS c
+ON h.cancoes_id = c.cancoes_id
+ORDER BY `usuario`, `nome`;
