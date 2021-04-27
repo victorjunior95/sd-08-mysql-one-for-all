60e47f050bf73960d7697c3634e5ff79a9b62143
Leonardo <lecastro16@gmail.com>
2021-04-27 19:17:39 -0300

    requisito 1 v5

--- desafio1.sql
+++ desafio1.sql
@@ -7,6 +7,10 @@ artista_id INT PRIMARY KEY AUTO_INCREMENT,
 artista VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB;
 
+INSERT INTO artistas(artista_id, artista)
+VALUES(1, "Walter Phoenix"),(2, "Peter Strong"),
+(3, "Lance Day"), (4, "Freedie Shannon");
+
 CREATE TABLE albuns(
 album_id INT PRIMARY KEY AUTO_INCREMENT,
 album VARCHAR(100) NOT NULL,
@@ -15,15 +19,24 @@ FOREIGN KEY(artista_id) REFERENCES artistas(artista_id)
 ) ENGINE=InnoDB;
 
 INSERT INTO albuns(album_id, album, artista_id)
-VALUES(1,"Envious"),(2,"Exuberant"), (3, "Hallowed Steam"),
-(4, "Incandescent"), (5, "Temporary Culture");
+VALUES(1,"Envious", 1),(2,"Exuberant", 1), (3, "Hallowed Steam", 2),
+(4, "Incandescent", 3), (5, "Temporary Culture", 4);
 
 CREATE TABLE musicas (
 musica_id INT PRIMARY KEY AUTO_INCREMENT,
-musica VARCHAR(200) NOT NULL,
+musica VARCHAR(250) NOT NULL,
 album_id INT NOT NULL,
 FOREIGN KEY(album_id) REFERENCES albuns(album_id)
 ) ENGINE=InnoDB;
+
+INSERT INTO musicas(musica_id, musica, album_id)
+VALUES(1,"Soul For Us", 1), (2, "Reflections Of Magic", 1),
+(3, "Dance With Her Own", 1), (4, "Troubles Of My Inner Fire", 2),
+(5, "Time Fireworks", 2), (6, "Magic Circus", 3), (7, "Honey, So Do I", 3),
+(8, "Sweetie, Let's Go Wild", 3), (9, "She Knows", 3), (10, "Fantasy For Me", 4),
+(11, "Celebration Of More", 4), (12, "Rock His Everything", 4), (13, "Home Forever", 4),
+(14, "Diamond Power", 4), (15, "Honey, Let's Be Silly", 4), (16, "Thang Of Thunder", 5),
+(17, "Words Of Her Life", 5), (18, "Without My Streets", 5);
 
 CREATE TABLE planos(
 plano_id INT PRIMARY KEY AUTO_INCREMENT,
