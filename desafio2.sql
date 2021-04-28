c5e7a3391360295be06a2478c3f2ba33d3addd10
Douglas Ferreira <dodofp.df@gmail.com>
2021-04-28 01:08:57 -0300

    Requisito 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,5 @@
-USE SpotifyClone;
-
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(*) AS cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.artistas) AS artistas,
 (SELECT COUNT(*) FROM SpotifyClone.albuns) AS albuns
-FROM cancoes;
+FROM SpotifyClone.cancoes;
