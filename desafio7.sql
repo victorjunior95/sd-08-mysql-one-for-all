ca77f01bb7a5fb24b5428f0f9c76f012745ac97a
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-04-27 23:34:45 -0300

    Complete challenges 7, 8, 9

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    AR.artist_name AS `artista`,
+    AL.album_name AS `album`,
+    COUNT(*) AS `seguidores`
+FROM
+    album AS AL
+        INNER JOIN
+    artist AS AR ON AR.artist_id = AL.artist_id
+        INNER JOIN
+    followed_artist AS F ON F.artist_id = AR.artist_id
+GROUP BY `album` , `artista`
+ORDER BY `seguidores` DESC , `artista` , `album`;
---
+++ desafio9.sql
@@ -0,0 +1,17 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista (IN artist_name VARCHAR(50))
+BEGIN
+SELECT 
+    AR.artist_name AS `atista`,
+    AL.album_name AS `album`
+FROM
+    album AS AL
+        INNER JOIN
+    artist AS AR ON AR.artist_id = AL.artist_id
+WHERE AR.artist_name = artist_name;
+
+END$$
+
+DELIMITER ;
