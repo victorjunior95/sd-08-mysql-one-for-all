54a3d45c1526432a67ad3b27f323aa354f2008da
Tandy <tandyfw@gmail.com>
2021-04-28 10:34:57 -0300

    Desafio 8 concluido-hotfix-1

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,12 @@
+USE SpotifyClone;
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico_de_reproducoes AS hdr WHERE hdr.usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.historico_reproducao_usuarios AS hru WHERE hru.usuario_id = OLD.usuario_id;
+DELETE hdr, sa FROM historico_de_reproducoes AS hdr
+JOIN seguindo_artistas AS sa
+ON sa.usuario_id = OLD.usuario_id
+WHERE hdr.usuario_id = OLD.usuario_id AND sa.usuario_id = OLD.usuario_id;
 END
 $$ DELIMITER ;
