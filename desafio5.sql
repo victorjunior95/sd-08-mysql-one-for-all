c49f075fdd228603e4778a11e3d81df999cdddbe
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-27 23:36:24 -0300

    Requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT C.nome_cancao cancao, COUNT(*) reproducoes
+FROM historico H
+INNER JOIN cancoes C ON C.cancao_id = H.cancao_id
+GROUP BY cancao
+ORDER BY reproducoes DESC LIMIT 2;
