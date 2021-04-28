7158ffd13688df750102cc906b4fdc0e5735299c
victor718 <victorfelipeoliveira1@gmail.com>
2021-04-28 00:18:39 -0300

    fix 0000

--- desafio1.sql
+++ desafio1.sql
@@ -1,37 +1,64 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
+
 USE SpotifyClone;
 
 CREATE TABLE plano (
-`id` INT NOT NULL AUTO_INCREMENT,
+`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 `tipo_plano` VARCHAR(45) NOT NULL,
-`valor_plano` FLOAT NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE INDEX `id_UNIQUE` (`id`) ,
-UNIQUE INDEX `tipo_plano_UNIQUE` (`tipo_plano`))
-ENGINE = InnoDB;
+`valor_plano` FLOAT NOT NULL
+)ENGINE = InnoDB;
+
+CREATE TABLE usuario (
+`id` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+`nome` VARCHAR(45) NOT NULL,
+`idade` INT NOT NULL,
+`plano_id` INT NOT NULL,
+FOREIGN KEY (`plano_id`) REFERENCES SpotifyClone.plano (`id`)
+)ENGINE = InnoDB;
+
+CREATE TABLE artista (
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nome_artista VARCHAR(45) NOT NULL
+)ENGINE = InnoDB;
+
+CREATE TABLE album (
+`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+`album_nome` VARCHAR(45) NOT NULL,
+`artista_id` INT NOT NULL,
+FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artista(id)
+)ENGINE = InnoDB;
+
+CREATE TABLE cancoes (
+`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+`nome_cancao` varchar(45) NOT NULL,
+`album_id` int NOT NULL,
+ FOREIGN KEY (`album_id`) REFERENCES album (`id`),
+ FOREIGN KEY (`artista_id`) REFERENCES artista (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE usuario_has_cancoes (
+`usuario_id` int NOT NULL,
+`cancoes_id` int NOT NULL,
+FOREIGN KEY (`cancoes_id`) REFERENCES `cancoes` (`id`),
+FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+PRIMARY KEY (`usuario_id`,`cancoes_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE usuario_artista (
+`usuario_id` INT NOT NULL,
+`artista_id` INT NOT NULL,
+FOREIGN KEY (`usuario_id`) REFERENCES SpotifyClone.usuario (`id`),
+FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artista (`id`),
+PRIMARY KEY (`usuario_id`, `artista_id`)
+) ENGINE = InnoDB;
 
 INSERT INTO plano
 (tipo_plano, valor_plano)
 VALUES("gratuito", 0),
 ("familiar", 7.99),
 ("universitário", 5.99);
-
-CREATE TABLE usuario (
-`id` INT NOT NULL AUTO_INCREMENT,
-`nome` VARCHAR(45) NOT NULL,
-`idade` INT NOT NULL,
-`plano_id` INT NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE INDEX `idusuario_UNIQUE` (`id`) ,
-INDEX `fk_usuario_plano_idx` (`plano_id`) ,
-CONSTRAINT `fk_usuario_plano`
-FOREIGN KEY (`plano_id`)
-REFERENCES `SpotifyClone`.`plano` (`id`)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 INSERT INTO usuario
 (nome, idade, plano_id)
@@ -40,35 +67,12 @@ VALUES("Thati", 23, 1),
 ("Bill", 20, 3),
 ("Roger", 45, 1);
 
-CREATE TABLE artista (
-id INT NOT NULL AUTO_INCREMENT,
-nome_artista VARCHAR(45) NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE INDEX `id_UNIQUE` (`id`),
-UNIQUE INDEX `nome_artista_UNIQUE` (`nome_artista`))
-ENGINE = InnoDB;
-
 INSERT INTO artista
 (nome_artista)
 VALUES("Walter Phoenix"),
 ("Peter Strong"),
 ("Lance Day"),
 ("Freedie Shannon");
-
-CREATE TABLE album (
-`id` INT NOT NULL AUTO_INCREMENT,
-`album_nome` VARCHAR(45) NOT NULL,
-`artista_id` INT NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE INDEX `id_UNIQUE` (`id`),
-UNIQUE INDEX `algum_UNIQUE` (`album_nome`),
-INDEX `fk_album_artista1_idx` (`artista_id`),
-CONSTRAINT `fk_album_artista1`
-FOREIGN KEY (`artista_id`)
-REFERENCES `SpotifyClone`.`artista` (`id`)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 INSERT INTO album
 (album_nome, artista_id)
@@ -77,54 +81,6 @@ VALUES("Envious", 1),
 ("Hallowed Steam", 2),
 ("Incandescent", 3),
 ("Temporary Culture", 4);
-
-CREATE TABLE usuario_artista (
-`usuario_id` INT NOT NULL,
-`artista_id` INT NOT NULL,
-PRIMARY KEY (`usuario_id`, `artista_id`),
-INDEX `fk_usuario_has_artista_artista1_idx` (`artista_id`),
-INDEX `fk_usuario_has_artista_usuario1_idx` (`usuario_id`),
-CONSTRAINT `fk_usuario_has_artista_usuario1`
-FOREIGN KEY (`usuario_id`)
-REFERENCES `SpotifyClone`.`usuario` (`id`)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION,
-CONSTRAINT `fk_usuario_has_artista_artista1`
-FOREIGN KEY (`artista_id`)
-REFERENCES `SpotifyClone`.`artista` (`id`)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-INSERT INTO usuario_artista
-(usuario_id, artista_id)
-VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 3),
-(2, 4),
-(2, 1),
-(3, 1),
-(3, 4),
-(3, 2),
-(4, 1),
-(4, 4),
-(4, 3);
-
-CREATE TABLE cancoes (
-`id` int NOT NULL AUTO_INCREMENT,
-`nome_cancao` varchar(45) NOT NULL,
-`artista_id` int NOT NULL,
-`album_id` int NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `id_UNIQUE` (`id`),
-UNIQUE KEY `nome_cancao_UNIQUE` (`nome_cancao`),
-KEY `fk_cancoes_artista1_idx` (`artista_id`),
-KEY `fk_cancoes_album1_idx` (`album_id`),
-CONSTRAINT `fk_cancoes_album1` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`),
-CONSTRAINT `fk_cancoes_artista1` FOREIGN KEY (`artista_id`) REFERENCES `artista` (`id`)
-) ENGINE=InnoDB;
 
 INSERT INTO cancoes
 (nome_cancao, artista_id, album_id)
@@ -148,16 +104,6 @@ VALUES
 ("Words Of Her Life", 4, 5),
 ("Without My Streets", 4, 5);
 
-CREATE TABLE usuario_has_cancoes (
-`usuario_id` int NOT NULL,
-`cancoes_id` int NOT NULL,
-PRIMARY KEY (`usuario_id`,`cancoes_id`),
-KEY `fk_usuario_has_cancoes_cancoes1_idx` (`cancoes_id`),
-KEY `fk_usuario_has_cancoes_usuario1_idx` (`usuario_id`),
-CONSTRAINT `fk_usuario_has_cancoes_cancoes1` FOREIGN KEY (`cancoes_id`) REFERENCES `cancoes` (`id`),
-CONSTRAINT `fk_usuario_has_cancoes_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB;
-
 INSERT INTO usuario_has_cancoes
 (usuario_id, cancoes_id)
 VALUES
@@ -175,3 +121,19 @@ VALUES
 (4, 3),
 (4, 18),
 (4, 11);
+
+INSERT INTO usuario_artista
+(usuario_id, artista_id)
+VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 3),
+(2, 4),
+(2, 1),
+(3, 1),
+(3, 4),
+(3, 2),
+(4, 1),
+(4, 4),
+(4, 3);
