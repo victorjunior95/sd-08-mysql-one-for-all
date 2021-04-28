ef557aae85855fc9d12d124121e939a892383ea6
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-27 22:09:54 -0300

    Requisito 2 - CodeClimate :/

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
-	SELECT
-	COUNT(*) 'cancoes',
-	(SELECT COUNT(*) FROM artistas) 'artistas',
-	(SELECT COUNT(*) FROM albuns) 'albuns'
-	FROM cancoes;
+SELECT
+COUNT(*) 'cancoes',
+(SELECT COUNT(*) FROM artistas) 'artistas',
+(SELECT COUNT(*) FROM albuns) 'albuns'
+FROM cancoes;
