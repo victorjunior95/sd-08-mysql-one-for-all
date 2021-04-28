e4e1dd5663339389352e2a3631137ed9766b6f6a
Leonardo <lecastro16@gmail.com>
2021-04-28 08:45:02 -0300

    requisito 2 v0

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(DISTINCT(m.musica_id)) AS `cancoes`,
+COUNT(DISTINCT(a.artista_id)) AS `artistas`,
+COUNT(DISTINCT(a.album_id)) AS `albuns`
+FROM SpotifyClone.musicas AS m
+INNER JOIN SpotifyClone.albuns AS a
+ON m.album_id = a.album_id;
