48713c780444790d5ff792f0c18d4fea66404645
Fernanda PC <nandateixmarques@gmail.com>
2021-04-27 21:43:54 -0300

    adicionando desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+CREATE VIEW top_3_artistas AS
+SELECT 
+a.nome AS 'artista',
+COUNT(s.usuario_id) AS 'seguidores'
+FROM artistas AS a
+INNER JOIN seguidores AS s
+ON s.artista_id = a.artista_id
+GROUP BY a.artista_id
+ORDER BY `seguidores` DESC, `artista` ASC;
