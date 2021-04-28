f76fd16dee2f4d26bba0ac3d414f907c8af466b3
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-27 21:07:52 -0300

    Challenge-8-done

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.followers
+    WHERE user_id = OLD.user_id;
+
+  DELETE FROM SpotifyClone.listening_history
+    WHERE user_id = OLD.user_id;
+END $$
+
+DELIMITER ;
