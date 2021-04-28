a2807cb0498fde25a47255b0e0625360e01f8b55
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-28 12:26:44 -0300

    Requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN parametro_de_entrada VARCHAR(50))
 BEGIN
-SELECT ART.artista AS artista, ALB.album AS album FROM artistas ART
+SELECT ART.nome_artista AS artista, ALB.nome_album AS album FROM artistas ART
 INNER JOIN albuns AS ALB ON ART.artista_id = ALB.artista_id
-WHERE ART.artista = parametro_de_entrada
+WHERE ART.nome_artista = parametro_de_entrada
 ORDER BY album;
 END $$
 DELIMITER ;
