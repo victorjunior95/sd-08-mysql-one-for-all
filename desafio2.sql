0ee68cba79cd65f63038e2481592614cac14cc58
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-27 22:03:19 -0300

    Requisito 2 - CodeClimate

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,5 @@
 SELECT
 COUNT(*) 'cancoes',
-    (SELECT
-		COUNT(*) FROM artistas) 'artistas',
-		(SELECT COUNT(*) FROM albuns) 'albuns'
+(SELECT COUNT(*) FROM artistas) 'artistas',
+(SELECT COUNT(*) FROM albuns) 'albuns'
 FROM cancoes;
-    
