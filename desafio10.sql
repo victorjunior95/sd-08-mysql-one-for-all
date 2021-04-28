701caf1a39e7e6c6980426aeadb802e19376d453
Cleberton <clebertonfgc@gmail.com>
2021-04-28 19:39:03 -0300

    Arequisitos 8 e 10 e ajustando/ renomeando arquivos

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,10 @@
-USE SpotifyClone;
-
 DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (id INT)
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE total INT; 
-  SELECT COUNT(id_cancao) FROM SpotifyClone.tb_historico_reproducoes WHERE id_historico_reproducoes = id INTO total;
+  SELECT COUNT(id_cancao) FROM SpotifyClone.tb_historico_reproducoes WHERE id_usuario = id INTO total;
   RETURN total;
 END $$
 
-DELIMITER ;
+SELECT * FROM SpotifyClone.tb_historico_reproducoes;
