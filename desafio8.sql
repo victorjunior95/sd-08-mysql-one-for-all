d6fbb087d9c956958170a1cc621b7ec1462b4305
Digo-Castro <dygo.castro@gmail.com>
2021-04-27 23:12:05 -0300

    [Desafio 10] - Round 2 - OK - Double Detroit Smash

--- desafio8.sql
+++ desafio8.sql
@@ -4,10 +4,10 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM historico AS h
-WHERE h.usuario_id = OLD.usuario_id;
-DELETE FROM seguindo AS s
-WHERE s.usuario_id = OLD.usuario_id;
+DELETE FROM historico
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM seguindo
+WHERE usuario_id = OLD.usuario_id;
 END
 $$
 DELIMITER ;
