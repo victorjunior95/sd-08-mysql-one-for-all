619f73990ad16d433831cfeac67ab84dbd086916
JRGCast <joao.ralph1@gmail.com>
2021-04-27 22:37:02 -0300

    inserido todos os valores já ordenados em criadorGeralzão

--- criadorGeralzao.sql
+++ criadorGeralzao.sql
@@ -18,8 +18,8 @@ PRIMARY KEY (album_id),
 FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO album (title) VALUES
-('Envious'), ('Exuberant'), ('Hallowed Steam'), ('Incandescent'), ('Temporary Culture');
+INSERT INTO album (title, artist_id) VALUES
+('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 4), ('Incandescent',3), ('Temporary Culture', 2);
 
 CREATE TABLE IF NOT EXISTS songs (
 song_id INT AUTO_INCREMENT,
@@ -29,12 +29,12 @@ PRIMARY KEY (song_id),
 FOREIGN KEY (album_id) REFERENCES album(album_id) 
 ) ENGINE=InnoDB;
 
-INSERT INTO songs (title) VALUES 
-("Soul For Us"), ("Reflections Of Magic"), ("Dance With Her Own"),
-("Troubles Of My Inner Fire"), ("Time Fireworks"),
-("Magic Circus"), ("Honey, So Do I"), ("Sweetie, Let's Go Wild"), ("She Knows"),
-("Fantasy For Me"), ("Celebration Of More"), ("Rock His Everything"), ("Home Forever"), ("Diamond Power"), ("Honey, Let's Be Silly"),
-("Thang Of Thunder"), ("Words Of Her Life"), ("Without My Streets");
+INSERT INTO songs (title, album_id) VALUES 
+("Soul For Us", 1), ("Reflections Of Magic", 1), ("Dance With Her Own", 1),
+("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2),
+("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3),
+("Fantasy For Me", 4), ("Celebration Of More", 4), ("Rock His Everything", 4), ("Home Forever", 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4),
+("Thang Of Thunder", 5), ("Words Of Her Life", 5), ("Without My Streets", 5);
 
 CREATE TABLE IF NOT EXISTS plans (
 plan_id INT AUTO_INCREMENT,
@@ -55,8 +55,8 @@ PRIMARY KEY (user_id),
 FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO users (username, user_age) VALUES 
-('Thati', 23), ('Cintia', 35), ('Bill',	20), ('Roger',45);
+INSERT INTO users (username, user_age, plan_id) VALUES 
+('Thati', 23, 1), ('Cintia', 35, 2), ('Bill', 20, 3), ('Roger', 45, 1);
 
 
 CREATE TABLE IF NOT EXISTS `following` (
@@ -66,9 +66,18 @@ FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 ) ENGINE=InnoDB;
 
+INSERT INTO `following` VALUES 
+(1,1), (1,2), (1,3), (2,1), (2,3), (3,1), (3,3), (3,4), (4,2);
+
 CREATE TABLE IF NOT EXISTS reproductions_history (
 user_id INT,
 song_id INT,
 FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (song_id) REFERENCES songs(song_id)
 ) ENGINE=InnoDB;
+
+INSERT INTO reproductions_history (user_id, song_id) VALUES
+  (1, 1), (1, 6), (1, 14), (1, 16),
+  (2, 13), (2, 17), (2, 2), (2, 15),
+  (3, 4), (3, 16), (3, 6), (4, 3),
+  (4, 18), (4, 11);
