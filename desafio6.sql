12cbccd4e52aad514f1d11b6d3eb0e4b20535a96
Rosiele David <rosieleserpa@hotmail.com>
2021-04-28 11:54:29 -0300

    Desafio 6 lint corrigido

--- desafio6.sql
+++ desafio6.sql
@@ -6,4 +6,3 @@ FORMAT(AVG(p.valor_plano), 2) AS 'faturamento_medio',
 FORMAT(SUM(p.valor_plano), 2) AS 'faturamento_total'
 FROM SpotifyClone.Plano AS p
 INNER JOIN SpotifyClone.Usuario AS u ON u.plano_id = p.plano_id;
-
