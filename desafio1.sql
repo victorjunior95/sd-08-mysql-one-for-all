cad84793848a1fdd7bb4cd58f0c20f5dff7b09be
Bruno Martins <bruno.martins217@gmail.com>
2021-04-28 05:19:44 -0300

    codeclimate fix3

--- desafio1.sql
+++ desafio1.sql
@@ -52,11 +52,11 @@ CREATE TABLE album(
 
 INSERT INTO album(album, singer_id)
 VALUES
-	('Envious', 1),
-  ('Exuberant', 1),
-  ('Halowed Steam', 2),
-  ('Incandescent', 3),
-  ('Temporary Culture', 4);
+('Envious', 1),
+('Exuberant', 1),
+('Halowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 CREATE TABLE music(
   music_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -67,7 +67,7 @@ CREATE TABLE music(
 
 INSERT INTO music(music, album_id)
 VALUES
-  ("Soul For Us", 1), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2), ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3), ("Fantasy For Me" , 4), ("Celebration Of More", 4), ("Rock His Everything", 4), ("Home Forever" , 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5), ("Words Of Her Life", 5), ("Without My Streets", 5);
+("Soul For Us", 1), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2), ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3), ("Fantasy For Me" , 4), ("Celebration Of More", 4), ("Rock His Everything", 4), ("Home Forever" , 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5), ("Words Of Her Life", 5), ("Without My Streets", 5);
 
 CREATE TABLE following(
   user_id INT NOT NULL,
@@ -79,10 +79,10 @@ CREATE TABLE following(
 
 INSERT INTO following(user_id, singer_id)
 VALUES
-  (1, 1), (1, 3), 
-  (1, 4), (2, 1),
-  (2, 3), (3, 1), 
-  (3, 2), (4, 4);
+(1, 1), (1, 3), 
+(1, 4), (2, 1),
+(2, 3), (3, 1), 
+(3, 2), (4, 4);
 
 CREATE TABLE hist_views(
   user_id INT NOT NULL,
@@ -94,10 +94,10 @@ CREATE TABLE hist_views(
 
 INSERT INTO hist_views(user_id, music_id)
 VALUES
-  (1, 1), (1, 6),
-  (1, 14), (1, 16),
-  (2, 13), (2, 17),
-  (2, 2), (2, 15),
-  (3, 4), (3, 16),
-  (3, 6), (4, 3),
-  (4, 18), (4, 11);
+(1, 1), (1, 6),
+(1, 14), (1, 16),
+(2, 13), (2, 17),
+(2, 2), (2, 15),
+(3, 4), (3, 16),
+(3, 6), (4, 3),
+(4, 18), (4, 11);
