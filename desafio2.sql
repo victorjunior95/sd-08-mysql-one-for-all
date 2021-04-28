ee1df5f0f5a47b0c25e871783914f3e1bcdac78f
Rosiele David <rosieleserpa@hotmail.com>
2021-04-28 01:11:34 -0300

    Desafio 2 concluído

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+CREATE VIEW estatisticas_musicais AS
+  SELECT COUNT(*) AS 'cancoes',
+  (
+    SELECT COUNT(*) FROM SpotifyClone.Artista
+  ) AS 'artistas',
+  (
+    SELECT COUNT(*) FROM SpotifyClone.Album
+  ) AS 'albuns'
+FROM SpotifyClone.Cancao AS c;
