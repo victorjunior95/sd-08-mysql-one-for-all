cadada11e18c021bd301e09f7e46e970e0549b6a
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-27 22:08:07 -0300

    Requisito 2 - Criando a View

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,6 @@
-SELECT
-COUNT(*) 'cancoes',
-(SELECT COUNT(*) FROM artistas) 'artistas',
-(SELECT COUNT(*) FROM albuns) 'albuns'
-FROM cancoes;
+CREATE VIEW estatisticas_musicais AS
+	SELECT
+	COUNT(*) 'cancoes',
+	(SELECT COUNT(*) FROM artistas) 'artistas',
+	(SELECT COUNT(*) FROM albuns) 'albuns'
+	FROM cancoes;
