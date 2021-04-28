4b00bdee1762f3cbdf9f345164472f3c5c335a1c
Rosiele David <rosieleserpa@hotmail.com>
2021-04-28 19:30:36 -0300

    Desafio 8 corrigido

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER trigger_usuario_delete
   BEFORE DELETE ON `Usuario`
   FOR EACH ROW
 BEGIN
-  DELETE FROM Usuario_Artista WHERE usuario_id = OLD.usuario_id;
-  DELETE FROM Usuario_Cancao WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.Usuario_Artista WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.Usuario_Cancao WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
