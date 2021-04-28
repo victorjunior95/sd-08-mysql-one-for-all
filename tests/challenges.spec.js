e96d17b6711143a684e194fb2af05074a51e0c06
Douglas Ferreira <dodofp.df@gmail.com>
2021-04-28 00:49:27 -0300

    Requisito 2

--- tests/challenges.spec.js
+++ tests/challenges.spec.js
@@ -29,7 +29,7 @@ describe('Queries de seleção', () => {
     sequelize.close();
   });
 
-  describe.only('1 - Normalize as tabelas para a 3ª Forma Normal', () => {
+  describe('1 - Normalize as tabelas para a 3ª Forma Normal', () => {
     const hasForeignKey = async (table, referencedTable) => {
       const [{ REFERENCE_COUNT: referenceCount }] = await sequelize.query(
         `SELECT COUNT(COLUMN_NAME) AS REFERENCE_COUNT
