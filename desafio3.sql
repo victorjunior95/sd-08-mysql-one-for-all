6cc02432bfeeca86e176ebd88ccc34c2835888b6
Thais Quintela <thais.s.quintela@gmail.com>
2021-04-28 04:30:58 -0300

    Requisitos 2 e 3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
+SELECT U.name AS 'usuario', M.name AS 'nome'
+FROM SpotifyClone.users AS U
+INNER JOIN SpotifyClone.historic AS H ON H.user_id = U.user_id
+INNER JOIN SpotifyClone.musics AS M ON M.music_id = H.music_id
+ORDER BY U.name, M.name;
