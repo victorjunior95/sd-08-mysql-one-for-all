04f70e10d4809b924195af79d80f4ad1bedb6fcc
Leonardo <lecastro16@gmail.com>
2021-04-28 09:14:11 -0300

    requisito 4 e arrumando requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,4 @@ INNER JOIN SpotifyClone.usuarios AS u
 ON um.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.musicas AS m
 ON um.musica_id = m.musica_id
-ORDER BY `usuario`;
+ORDER BY `usuario`, `nome`;
---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_3_artistas AS
+SELECT a.artista AS `artista`,COUNT(ua.usuario_id) AS `seguidores`
+FROM SpotifyClone.usuarios_artistas AS ua
+INNER JOIN SpotifyClone.artistas AS a
+ON ua.artista_id = a.artista_id
+GROUP BY(ua.artista_id)
+ORDER BY `seguidores` DESC, `artista`;
