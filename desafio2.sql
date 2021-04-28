546e5e0bbedcc5621416a79e6cbf32eda174ed46
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-04-27 21:02:32 -0300

    Complete challenges 2, 3

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        (SELECT 
+                COUNT(*)
+            FROM
+                song) AS `cancoes`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                artist) AS `artistas`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                album) AS `albuns`;
