4fdac720d63635479819c0d144c422c05b74bb96
Cleberton <clebertonfgc@gmail.com>
2021-04-28 09:12:29 -0300

    Desafios 1 e 2 Ajustando code Climate

--- SpotifyClone.sql
+++ SpotifyClone.sql
@@ -60,24 +60,10 @@ INSERT INTO tb_plano (nome, valor) VALUES ('gratuito', 0.0), ('familiar', 7.99),
 INSERT INTO tb_usuario (nome, idade, id_plano) VALUES ('Thati', 23, 1), ('Cintia', 35, 2), ('Bill', 20, 3), ('Roger', 45, 1);
 INSERT INTO tb_artista (nome) VALUES ('Walter Phoenix'),('Peter Strong'), ('Lance Day'),('Freedie Shannon');
 INSERT INTO tb_album (nome, id_artista) VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
-INSERT INTO tb_cancao (nome, id_album) VALUES ('Soul For Us', 1),
-											 ('Reflections Of Magic', 1),
- 											 ('Dance With Her Own',1),
- 											 ('Troubles Of My Inner Fire', 2),
-                                              ('Time Fireworks', 2),
-                                              ('Magic Circus', 3),
-                                              ('Honey, So Do I',3),
-                                              ('Sweetie, Let\'s Go Wild',3),
-                                              ('She Knows',3),
-                                              ('Fantasy For Me',4),
-                                              ('Celebration Of More', 4),
-                                              ('Rock His Everything',4),
-                                              ('Home Forever', 4),
-                                              ('Diamond Power',4 ),
-                                             ('Honey, Let\'s Be Silly', 4),
-                                              ('Thang Of Thunder', 5),
-                                            ('Words Of Her Life', 5),
-                                             ('Without My Streets',5);
+INSERT INTO tb_cancao (nome, id_album) VALUES ('Soul For Us', 1),('Reflections Of Magic', 1), ('Dance With Her Own',1),
+  ('Troubles Of My Inner Fire', 2), ('Time Fireworks', 2), ('Magic Circus', 3), ('Honey, So Do I',3), ('Sweetie, Let\'s Go Wild',3),
+  ('She Knows',3), ('Fantasy For Me',4), ('Celebration Of More', 4), ('Rock His Everything',4), ('Home Forever', 4),
+  ('Diamond Power',4 ), ('Honey, Let\'s Be Silly', 4), ('Thang Of Thunder', 5), ('Words Of Her Life', 5), ('Without My Streets',5);
                                              
 INSERT INTO tb_historico_reproducoes(id_historico_reproducoes, id_usuario, id_cancao) VALUES (1,1,1),(1,1,6),(1,1,14),(1,1,16);
 INSERT INTO tb_historico_reproducoes(id_historico_reproducoes, id_usuario, id_cancao) VALUES (2,2,13),(2,2,17),(2,2,2),(2,2,15);
