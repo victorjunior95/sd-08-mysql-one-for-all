2d1b0a2cdc77bf449f52499d337875dfe136c1da
Leonardo <lecastro16@gmail.com>
2021-04-28 13:58:55 -0300

    requisito 11 v2

--- desafio11.sql
+++ desafio11.sql
@@ -5,16 +5,6 @@ INNER JOIN SpotifyClone.musicas AS m
 ON um.musica_id = m.musica_id
 INNER JOIN SpotifyClone.usuarios AS u
 ON um.usuario_id = u.usuario_id
+WHERE u.plano_id IN (2,3)
 GROUP BY m.musica
 ORDER BY `nome`;
-
-
-SELECT m.musica AS `nome`, COUNT(um.usuario_id) AS `reproducoes`
-FROM SpotifyClone.usuarios_musicas AS um
-INNER JOIN SpotifyClone.musicas AS m
-ON um.musica_id = m.musica_id
-INNER JOIN SpotifyClone.usuarios AS u
-ON um.usuario_id = u.usuario_id
-GROUP BY m.musica
-ORDER BY `nome`;
-
