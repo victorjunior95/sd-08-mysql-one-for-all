088884d5be92aae94ee59c21d209628a6f030414
Bruno Martins <bruno.martins217@gmail.com>
2021-04-29 16:03:08 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
+SELECT
+COUNT(DISTINCT m.music) AS cancoes,
+COUNT(DISTINCT s.singer) AS artistas,
+COUNT(DISTINCT a.album) AS albuns
+FROM 
+SpotifyClone.album AS a,
+SpotifyClone.singer AS s,
+SpotifyClone.music as m;
