df50518d178d8f9801ec5c125f7829685151c82a
albertassi88 <albertassi88@gmail.com>
2021-04-28 22:09:45 -0300

    Alteracoes no exercicio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,12 @@
 USE SpotifyClone;
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(id_user INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE id_music INT;
-SELECT COUNT(h.usuario_id)
-FROM SpotifyClone.historico_reproducoes AS h
-INNER JOIN SpotifyClone.usuario AS u
-ON u.usuario_id = h.usuario_id
-WHERE u.nome_usuario = nome INTO id_music;
+SELECT COUNT(*)
+FROM SpotifyClone.historico_reproducoes
+WHERE usuario_id = id_user INTO id_music;
 RETURN id_music;
 END $$
 DELIMITER ;
