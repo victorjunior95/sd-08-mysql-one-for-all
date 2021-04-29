686f35976eb632c1260088370d1564c0eee71478
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-29 06:51:27 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        COUNT(m.music_id) AS cancoes,
+        (SELECT 
+                COUNT(a.artiste_id)
+            FROM
+                SpotifyClone.artiste AS a) AS artistas,
+        (SELECT 
+                COUNT(alb.album_id)
+            FROM
+                SpotifyClone.albums AS alb) AS albuns
+    FROM
+        SpotifyClone.musics AS m;
+    
