7d1e18519409e3669a7a98dde3ac6f30dd5b3912
antonioarieiro <sfgfffs@gmail.com>
2021-04-29 15:20:16 -0300

    re 2 ident

--- images/desafio2.sql
+++ images/desafio2.sql
@@ -1,6 +1,15 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT 
-    (SELECT COUNT(*) FROM song) AS `cancoes`,
-    (SELECT COUNT(*) FROM artist) AS `artistas`,
-    (SELECT COUNT(*) FROMalbum) AS `albuns`;
-    
+    (SELECT 
+                COUNT(*)
+            FROM
+                song) AS `cancoes`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                artist) AS `artistas`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                album) AS `albuns`;
+                
