9c3786b946db56613c50716029f59e6e65cb2128
Leandro Lachini <leandrolachini@gmail.com>
2021-04-29 18:53:44 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico_de_reproducoes AS hp WHERE hp.usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.seguindo_artista AS ar WHERE ar.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artista WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
