5ebbfbd3bf52c106f6b36be4b4a2c29f0861f38d
José Bernardo Johann <josebernardojohann@gmail.com>
2021-04-29 20:21:12 -0300

    Fixing syntax errors

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 CREATE VIEW estatisticas_musicais AS
-  SELECT
-    COUNT(S.song) AS cancoes,
-    COUNT(DISTINCT A.artist) AS artistas,
-    COUNT(DISTINCT AL.album) AS albums
-  FROM SpotifyClone.songs AS S
-INNER JOIN SpotifyClone.albums AS AL ON AL.album_id = S.album_id;
-INNER JOIN SpotifyClone.artists AS A ON A.artist_id = AL.artist_id
+SELECT
+  COUNT(c.cancao) AS cancoes,
+  COUNT(DISTINCT A.artist) AS artistas,
+  COUNT(DISTINCT AL.album) AS albuns
+FROM SpotifyClone.songs AS S
+INNER JOIN SpotifyClone.albums AS AL ON AL.album_id = S.album_id
+INNER JOIN SpotifyClone.artists AS A ON A.artist_id = AL.artist_id;
