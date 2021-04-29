bd2e15335c60da9dda23f7921fd5a7900b6e09bb
Victor Miwa <victoryukimiwa@hotmail.com>
2021-04-28 22:31:53 -0300

    Desafio 11 (create view)

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+CREATE VIEW cancoes_premium AS 
+  SELECT SONG.song, COUNT(SONG.song) FROM SpotifyClone.User AS `USER`
+  INNER JOIN SpotifyClone.Historic AS `HISTORIC`
+  ON USER.user_id = HISTORIC.user_id
+  INNER JOIN SpotifyClone.Song AS `SONG`
+  ON HISTORIC.song_id = SONG.song_id
+  WHERE USER.plan_id IN (2, 3)
+  GROUP BY SONG.song, HISTORIC.user_id
+  ORDER BY SONG.song;
