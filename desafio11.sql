f23679fe40f10f992d6a3c7d85702d9d36288576
Leandro Lachini <leandrolachini@gmail.com>
2021-04-29 19:13:01 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,4 @@
--- CREATE VIEW cancoes_premium AS
-
-SELECT
-c.nome_cancao AS `nome`,
-COUNT(hp.usuario_id) AS `reproducoes`
-FROM SpotifyClone.historico_de_reproducoes AS hp
-INNER JOIN SpotifyClone.cancoes AS c
-ON hp.cancao_id = c.cancao_id
-GROUP BY `nome`
-ORDER BY `nome`;
-
+CREATE VIEW cancoes_premium AS
 SELECT
 c.nome_cancao AS `nome`,
 COUNT(hp.usuario_id) AS `reproducoes`
@@ -19,4 +9,6 @@ INNER JOIN SpotifyClone.usuario AS u
 ON hp.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.plano AS p
 ON u.plano_id = p.plano_id
-WHERE p.plano = 'familiar' OR p.plano = 'universitário';
+WHERE p.plano = 'familiar' OR p.plano = 'universitário'
+GROUP BY hp.cancao_id
+ORDER BY `nome`;
