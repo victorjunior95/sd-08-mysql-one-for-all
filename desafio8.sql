12a089508d805bd063ca283929c847b748017961
Thais Quintela <thais.s.quintela@gmail.com>
2021-04-29 15:54:52 -0300

    Correcao

--- desafio8.sql
+++ desafio8.sql
@@ -3,13 +3,13 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-    BEFORE DELETE ON `users`
+    BEFORE DELETE ON users
     FOR EACH ROW
 BEGIN
-    DELETE FROM `following`
+    DELETE FROM following
     WHERE user_id = OLD.user_id;
 
-    DELETE FROM `historic`
+    DELETE FROM historic
     WHERE user_id = OLD.user_id;
 END $$
 
