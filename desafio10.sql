d545e53d32a9a49bba1de28943eed327c784451f
Leandro Lachini <leandrolachini@gmail.com>
2021-04-28 21:20:10 -0300

    desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico (usuario VARCHAR(30))
+CREATE FUNCTION quantidade_musicas_no_historico (usuarioId INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE searchResult INT;
@@ -8,9 +8,9 @@ SELECT COUNT(*) AS `quantidade_musicas_no_historico`
 FROM SpotifyClone.historico_de_reproducoes AS hp
 INNER JOIN SpotifyClone.usuario AS u
 ON hp.usuario_id = u.usuario_id
-WHERE u.nome = usuario INTO searchResult;
+WHERE hp.usuario_id = usuarioId INTO searchResult;
 RETURN searchResult;
 END $$
 DELIMITER ;
 
-SELECT quantidade_musicas_no_historico('BILL');
+SELECT quantidade_musicas_no_historico(3);
