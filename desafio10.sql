936c44f2e7cb2f126730c1edb469e09264eeb92e
albertassi88 <albertassi88@gmail.com>
2021-04-28 21:47:23 -0300

    Alteracoes no exercicio 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE id_music INT;
-SELECT COUNT(h.usuario_id) AS 'quantidade_musicas_no_historico'
+SELECT COUNT(h.usuario_id)
 FROM SpotifyClone.historico_reproducoes AS h
 INNER JOIN SpotifyClone.usuario AS u
 ON u.usuario_id = h.usuario_id
@@ -12,3 +12,10 @@ WHERE u.nome_usuario = nome INTO id_music;
 RETURN id_music;
 END $$
 DELIMITER ;
+
+
+
+
+
+
+
