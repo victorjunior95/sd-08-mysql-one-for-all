60035fc28a0ef50d5047dd610cb0ba6c1891f4e3
Fernanda PC <nandateixmarques@gmail.com>
2021-04-30 16:14:01 -0300

    adicionando desafio 11 - corrigindo

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,10 @@ COUNT(hr.cancao_id) AS 'reproducoes'
 FROM SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.historico_de_reproducoes AS hr
 ON hr.cancao_id = c.cancao_id
+INNER JOIN SpotifyClone.usuarios AS u
+ON u.usuario_id = hr.usuario_id
+INNER JOIN SpotifyClone.planos AS p
+ON p.plano_id = u.id_plano
+WHERE p.plano IN('familiar', 'universitário')
 GROUP BY c.nome
 ORDER BY `nome` ASC;
