9a40297e9ee7bb6775cd26f1373efcff1995c6dd
JRGCast <joao.ralph1@gmail.com>
2021-04-29 23:21:25 -0300

    alterada contagem de userID para contagem total, a fim de haver melhor legibilidade e congruencia com a funçao no desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ USE SpotifyClone;
 DROP FUNCTION quantidade_musicas_no_historico;
 DELIMITER $$ CREATE FUNCTION quantidade_musicas_no_historico(id_user INT) RETURNS INT READS SQL DATA BEGIN
 DECLARE total_songs INT;
-SELECT COUNT(user_id)
+SELECT COUNT(*)
 FROM reproductions_history AS rh
 WHERE rh.user_id = id_user INTO total_songs;
 RETURN total_songs;
