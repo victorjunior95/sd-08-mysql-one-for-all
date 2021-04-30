2919de3103c1f5647f01b84620d39963d0f19c42
JRGCast <joao.ralph1@gmail.com>
2021-04-29 23:18:22 -0300

    trocada tabela de referencia do desafio 10 de users para reproductions history

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,9 @@
 USE SpotifyClone;
-
-DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(id_user VARCHAR(50))
-RETURNS INT READS SQL DATA
-BEGIN
+DROP FUNCTION quantidade_musicas_no_historico;
+DELIMITER $$ CREATE FUNCTION quantidade_musicas_no_historico(id_user INT) RETURNS INT READS SQL DATA BEGIN
 DECLARE total_songs INT;
-SELECT COUNT(user_id) 
-FROM users 
-WHERE users.user_id = id_user INTO total_songs;
-RETURN total_songs; 
-END $$
-DELIMITER ;
+SELECT COUNT(user_id)
+FROM reproductions_history AS rh
+WHERE rh.user_id = id_user INTO total_songs;
+RETURN total_songs;
+END $$ DELIMITER;
