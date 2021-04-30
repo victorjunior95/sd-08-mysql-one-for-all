187396551defe2537b40979b092b66a1a3aab586
UmberBn <humberto-bandeira@hotmail.com>
2021-04-29 22:14:46 -0400

    Nem pra enviar o projeto o pobre tem paz

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON Users
+    BEFORE DELETE ON Users
     FOR EACH ROW
 BEGIN
-	DELETE FROM HistoricoDeMusicas
+    DELETE FROM HistoricoDeMusicas
     WHERE idUser = OLD.idUser;
     DELETE FROM Seguindo
     WHERE idUser = OLD.idUser;
--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN name VARCHAR(45))
 BEGIN
-	SELECT AR.Nome AS `artista`,
+    SELECT AR.Nome AS `artista`,
     AB.NOME AS `album`
     FROM Albuns AS AB
     INNER JOIN Artistas AS AR
