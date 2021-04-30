f71507f16c29e20e08f3f31fd6ec4449e91cf3cd
bidfca <bruninhakjurugat11@gmail.com>
2021-04-30 10:28:01 -0300

    Desafio4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT artista.artista,
+COUNT(*) AS seguidores
+FROM SpotifyClone.seguindo_artistas sartista
+INNER JOIN SpotifyClone.artistas artista
+ON sartista.artista_id = artista.artista_id
+GROUP BY artista.artista;
+
