e0f2634fb153ad3e7e678b41c4fdca8b041f5ded
bidfca <bruninhakjurugat11@gmail.com>
2021-04-30 09:14:36 -0300

    Code Climate Fix

--- desafio1.sql
+++ desafio1.sql
@@ -93,22 +93,22 @@ VALUES
 INSERT INTO
     SpotifyClone.cancoes(cancao, album_id)
 VALUES
-	("Soul For Us", 1),
+("Soul For Us", 1),
     ("Reflections Of Magic", 1),
     ("Dance With Her Own", 1),
-	("Troubles Of My Inner Fire", 2),
+("Troubles Of My Inner Fire", 2),
     ("Time Fireworks", 2),
-	("Magic Circus", 3),
+("Magic Circus", 3),
     ("Honey, So Do I", 3),
     ("Sweetie, Let's Go Wild", 3),
     ("She Knows", 3),
-	("Fantasy For Me", 4),
+("Fantasy For Me", 4),
     ("Celebration Of More", 4),
     ("Rock His Everything", 4),
     ("Home Forever", 4),
     ("Diamond Power", 4),
     ("Honey, Let's Be Silly", 4),
-	("Thang Of Thunder", 5),
+("Thang Of Thunder", 5),
     ("Words Of Her Life", 5),
     ("Without My Streets", 5);
 
