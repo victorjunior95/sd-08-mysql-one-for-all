cfd050d6426b0e3c9c8ef73404c7b1420025a824
bidfca <bruninhakjurugat11@gmail.com>
2021-04-30 13:12:55 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT cancao.cancao,
+COUNT(*) AS reproducoes
+FROM SpotifyClone.cancoes cancao
+INNER JOIN SpotifyClone.historico_de_reproducoes hreproducao
+ON hreproducao.cancao_id = cancao.cancao_id
+GROUP BY cancao.cancao
+ORDER BY COUNT(*) DESC
+LIMIT 2;
