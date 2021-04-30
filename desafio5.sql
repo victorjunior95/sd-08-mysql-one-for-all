62e29056fe4dad869a4634cc0ad7d55f4452ae26
Fernanda PC <nandateixmarques@gmail.com>
2021-04-30 10:49:43 -0300

    adicionando desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+use SpotifyClone;
+CREATE VIEW top_2_hits_do_momento AS
+SELECT 
+c.nome AS 'cancao',
+COUNT(hr.usuario_id) AS 'reproducoes'
+FROM cancoes AS c
+INNER JOIN historico_de_reproducoes AS hr
+ON hr.cancao_id = c.cancao_id
+GROUP BY c.cancao_id
+ORDER BY `reproducoes` DESC, `cancao` ASC
+LIMIT 2;
