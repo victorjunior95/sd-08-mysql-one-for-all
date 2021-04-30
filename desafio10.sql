1febbc09e01bdc5fa3612d40ea4ae7061505624a
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 18:47:37 -0300

    fix: test remove name duplicate

--- desafio10.sql
+++ desafio10.sql
@@ -4,11 +4,11 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE quantidade_de_musicas_historico INT;
-    SELECT COUNT(cancao_id)
+    DECLARE quantidade_de_musicas INT;
+    SELECT COUNT(*)
     FROM historico_de_reproducoes
     WHERE usuario_id = id INTO quantidade_de_musicas_historico;
-    RETURN quantidade_de_musicas_historico;
+    RETURN quantidade_de_musicas;
 END $$
 DELIMITER ;
 
