a9e0fc54544f5b3de48c04cac83a63dfed97c7f2
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-30 18:49:29 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,25 @@
+CREATE VIEW cancoes_premium AS
+	SELECT 
+		(SELECT 
+				m.music_name
+			FROM
+				SpotifyClone.musics AS m
+			WHERE
+				m.music_id = h.music_id) AS nome,
+		COUNT(*) AS reproducoes
+	FROM
+		SpotifyClone.historic AS h
+	WHERE
+		h.user_id IN (SELECT 
+				u.user_id
+			FROM
+				SpotifyClone.users AS u
+			WHERE
+				u.pass_id IN (SELECT 
+						p.pass_id
+					FROM
+						SpotifyClone.pass AS p
+					WHERE
+						p.pass_name IN ('familiar' , 'universitário')))
+	GROUP BY h.music_id
+	ORDER BY nome;
