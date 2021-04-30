5ce1de7551f8c7925fc333b1db67ff609f36350b
Bruno Martins <bruno.martins217@gmail.com>
2021-04-29 22:51:19 -0300

    Desafio 6 - fix

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT
 MIN(p.plan_value) AS faturamento_minimo,
 MAX(p.plan_value) AS faturamento_maximo,
-FORMAT(AVG(p.plan_value), 2) AS faturamento_medio,
+FORMAT(AVG(CEIL(p.plan_value)), 2) AS faturamento_medio,
 FORMAT(SUM(p.plan_value), 2) AS faturamento_total
 FROM 
 SpotifyClone.plan AS p
