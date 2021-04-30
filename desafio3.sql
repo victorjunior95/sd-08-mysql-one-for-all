4b3372e37966ffcd357b855079256a35335c4f17
Giovanna Betti <giovannabetti@gmail.com>
2021-04-30 01:51:26 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+SELECT 
+    U.name AS 'usuario', S.song AS 'nome'
+FROM
+    SpotifyClone.users U
+        INNER JOIN
+    SpotifyClone.played_music_history P ON U.user_id = P.user_id
+        INNER JOIN
+    SpotifyClone.songs S ON P.song_id = S.song_id
+ORDER BY `usuario` , `nome`;
