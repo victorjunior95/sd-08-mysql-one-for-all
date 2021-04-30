093a7ea45ea5a92b7296c4f919a9c3f5ea811bab
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 17:24:41 -0300

    fix: added view

--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,4 @@
+CREATE VIEW top_3_artistas AS
 SELECT sa.artista AS 'artista', COUNT(sa.artista_id) AS 'seguidores' FROM SpotifyClone.seguidores AS ss
 INNER JOIN SpotifyClone.artistas AS sa
 ON ss.artista_id = sa.artista_id
