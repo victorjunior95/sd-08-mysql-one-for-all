662cf96240858ade4c94ff34c407938f44afed4b
bidfca <bruninhakjurugat11@gmail.com>
2021-04-30 10:43:44 -0300

    Fix Again :(

--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,4 @@
+DROP VIEW IF EXISTS top_3_artistas;
 CREATE VIEW top_3_artistas AS
 SELECT artista.artista,
 COUNT(*) AS seguidores
@@ -5,4 +6,5 @@ FROM SpotifyClone.seguindo_artistas sartista
 INNER JOIN SpotifyClone.artistas artista
 ON sartista.artista_id = artista.artista_id
 GROUP BY artista.artista
+ORDER BY COUNT(*) DESC
 LIMIT 3;
