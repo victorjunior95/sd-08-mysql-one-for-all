a0f2dc2e934ce900d19aaf409b2ca2abfef4fead
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 18:11:31 -0300

    feat: added requeriment 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(usuario_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE quantidade_de_musicas_historico INT;
+    SELECT COUNT(cancao_id)
+    FROM SpotifyClone.historico_de_reproducoes AS shr
+    WHERE shr.usuario_id = usuario_id INTO quantidade_de_musicas_historico;
+    RETURN quantidade_de_musicas_historico;
+END $$
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
