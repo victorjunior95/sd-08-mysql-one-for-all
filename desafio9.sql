6d7a5e4a6a80abf4abad73401dd4ea46652764fb
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-30 15:55:27 -0300

    desafio 9 - codeclimate 3

--- desafio9.sql
+++ desafio9.sql
@@ -5,21 +5,21 @@ CREATE PROCEDURE albuns_do_artista(IN artiste VARCHAR(25))
 BEGIN
 SELECT 
 (SELECT 
-		art.artiste_name
-	FROM
-		SpotifyClone.artiste AS art
-	WHERE
-		alb.artiste_id = art.artiste_id) AS artista,
+	art.artiste_name
+FROM
+	SpotifyClone.artiste AS art
+WHERE
+	alb.artiste_id = art.artiste_id) AS artista,
 alb.album_name AS album
 FROM
 SpotifyClone.albums AS alb
 WHERE
 (SELECT 
-		art.artiste_name
-	FROM
-		SpotifyClone.artiste AS art
-	WHERE
-		alb.artiste_id = art.artiste_id) = artiste
+	art.artiste_name
+FROM
+	SpotifyClone.artiste AS art
+WHERE
+	alb.artiste_id = art.artiste_id) = artiste
 ORDER BY album;
 END
 $$ DELIMITER ;
