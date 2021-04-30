d73584ab26f2d289273d7f0354620c491c11187e
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 19:23:09 -0300

    fix: test refactory query

--- desafio10.sql
+++ desafio10.sql
@@ -6,10 +6,8 @@ RETURNS INT READS SQL DATA
 BEGIN
     DECLARE quantidade_de_musicas INT;
     SELECT COUNT(*)
-    FROM historico_de_reproducoes
+    FROM SpotifyClone.historico_de_reproducoes
     WHERE usuario_id = id INTO quantidade_de_musicas;
     RETURN quantidade_de_musicas;
 END $$
 DELIMITER ;
-
-SELECT quantidade_musicas_no_historico(3);
--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-BEFORE DELETE ON usuarios
+BEFORE DELETE ON `usuarios`
 FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.historico_de_reproducoes as shr
