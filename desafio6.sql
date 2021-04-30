7cd89ea9a360cc7352834674459ea98b5309dd47
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 18:11:05 -0300

    fix: error word

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,8 @@
 CREATE VIEW faturamento_atual AS
-SELECT 
-ROUND(MIN(p.valor_plano), 2) AS 'faturamento_minimo',
-ROUND(MAX(p.valor_plano), 2) AS 'faturamento_maximo',
-ROUND(AVG(p.valor_plano), 2) AS 'faturamento_medio',
-ROUND(SUM(p.valor_plano), 2) AS 'faturamento_total'
+SELECT ROUND(MIN(sp.valor_plano), 2) AS 'faturamento_minimo',
+ROUND(MAX(sp.valor_plano), 2) AS 'faturamento_maximo',
+ROUND(AVG(sp.valor_plano), 2) AS 'faturamento_medio',
+ROUND(SUM(sp.valor_plano), 2) AS 'faturamento_total'
 FROM SpotifyClone.planos AS sp
 INNER JOIN SpotifyClone.usuarios AS su
-ON su.id_plano = sp.plano_id;
+ON sp.plano_id = su.plano_id;
