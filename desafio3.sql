781a7e1a7328f6055bcdf0ccbf48e53814ac8e27
Giovanna Betti <giovannabetti@gmail.com>
2021-04-30 01:54:50 -0300

    corrigindo 2 e 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,10 @@
-SELECT 
-    U.name AS 'usuario', S.song AS 'nome'
-FROM
-    SpotifyClone.users U
-        INNER JOIN
-    SpotifyClone.played_music_history P ON U.user_id = P.user_id
-        INNER JOIN
-    SpotifyClone.songs S ON P.song_id = S.song_id
-ORDER BY `usuario` , `nome`;
+CREATE VIEW `historico_reproducao_usuarios` AS
+    SELECT 
+        U.name AS 'usuario', S.song AS 'nome'
+    FROM
+        SpotifyClone.users U
+            INNER JOIN
+        SpotifyClone.played_music_history P ON U.user_id = P.user_id
+            INNER JOIN
+        SpotifyClone.songs S ON P.song_id = S.song_id
+    ORDER BY `usuario` , `nome`;
