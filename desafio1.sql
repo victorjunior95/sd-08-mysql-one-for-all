378e2f8bef47a727bbab72c23e1d69c9d976a8f2
Ana Karine <anakarine.ti@gmail.com>
2021-04-30 20:47:50 -0300

    CC

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,6 @@
+-- Colaboração:
+-- Arnaelcio: https://github.com/Arnaelcio
+-- Marcelo: https://github.com/Marcelllombm
 DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
@@ -12,9 +15,9 @@ CREATE TABLE planos (
 
 INSERT INTO planos (nome, valor)
 VALUES
-	('gratuito', 0),
-    ('familiar', 7.99),
-    ('universitário', 5.99);
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
     
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -27,10 +30,10 @@ CREATE TABLE usuarios (
 
 INSERT INTO usuarios (nome, idade, plano_id)
 VALUES
-	('Thati', 23, 1),
-    ('Cintia', 35, 2),
-    ('Bill', 20, 3),
-    ('Roger', 45, 1);
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
 	
 CREATE TABLE artistas (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -39,10 +42,10 @@ CREATE TABLE artistas (
     
 INSERT INTO artistas (nome)
 VALUES
-	('Walter Phoenix'),
-    ('Peter Strong'),
-    ('Lance Day'),
-    ('Freedie Shannon');
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
 CREATE TABLE albuns (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -54,11 +57,11 @@ CREATE TABLE albuns (
 
 INSERT INTO albuns (nome, artista_id)
 VALUES
-	('Envious', 1),
-    ('Exuberant', 1),
-    ('Hallowed Steam', 2),
-    ('Incandescent', 3),
-    ('Temporary Culture', 4);
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 CREATE TABLE musicas (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -70,24 +73,24 @@ CREATE TABLE musicas (
 
 INSERT INTO musicas (nome, album_id)
 VALUES
-	("Soul For Us", 1),
-	("Reflections Of Magic", 1),
-	("Dance With Her Own", 1),
-	("Troubles Of My Inner Fire", 2),
-	("Time Fireworks", 2),
-	("Magic Circus", 3),
-	("Honey, So Do I", 3),
-	("Sweetie, Let's Go Wild", 3),
-	("She Knows", 3),
-	("Fantasy For Me", 4),
-	("Celebration Of More", 4),
-	("Rock His Everything", 4),
-	("Home Forever", 4),
-	("Diamond Power", 4),
-	("Honey, Let's Be Silly", 4),
-	("Thang Of Thunder", 5),
-	("Words Of Her Life", 5),
-	("Without My Streets", 5);
+("Soul For Us", 1),
+("Reflections Of Magic", 1),
+("Dance With Her Own", 1),
+("Troubles Of My Inner Fire", 2),
+("Time Fireworks", 2),
+("Magic Circus", 3),
+("Honey, So Do I", 3),
+("Sweetie, Let's Go Wild", 3),
+("She Knows", 3),
+("Fantasy For Me", 4),
+("Celebration Of More", 4),
+("Rock His Everything", 4),
+("Home Forever", 4),
+("Diamond Power", 4),
+("Honey, Let's Be Silly", 4),
+("Thang Of Thunder", 5),
+("Words Of Her Life", 5),
+("Without My Streets", 5);
 
 CREATE TABLE seguidores (
     id INT AUTO_INCREMENT,
@@ -102,14 +105,14 @@ CREATE TABLE seguidores (
 
 INSERT INTO seguidores (usuario_id, artista_id)
 VALUES
-	(1, 1),
-	(1, 4),
-	(1, 3),
-	(2, 1),
-	(2, 3),
-	(3, 2),
-	(3, 1),
-	(4, 4);
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
 
 CREATE TABLE historico (
     id INT AUTO_INCREMENT,
@@ -124,17 +127,17 @@ CREATE TABLE historico (
 
 INSERT INTO historico (usuario_id, musica_id)
 VALUES
-	(1, 1),
-	(1, 6),
-	(1, 14),
-	(1, 16),
-	(2, 13),
-	(2, 17),
-	(2, 2),
-	(2, 7),
-	(3, 4),
-	(3, 16),
-	(3, 6),
-	(4, 3),
-	(4, 18),
-	(4, 11);
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 7),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
