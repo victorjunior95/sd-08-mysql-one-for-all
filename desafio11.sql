8bf104360229463509fc24c9c4f00f30ca978fce
MEianelli <filipelouzano@gmail.com>
2021-04-30 15:57:01 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,18 @@
+CREATE VIEW cancoes_premium AS
+SELECT 
+a.song_name AS 'nome', COUNT(b.song_id) AS 'reproducoes' 
+FROM SpotifyClone.songs a
+INNER JOIN (
+SELECT song_id 
+FROM spotifyclone.historico_de_reproducoes 
+WHERE user_id IN (
+SELECT user_id 
+FROM SpotifyClone.users 
+WHERE plano_id IN (
+SELECT plano_id 
+FROM SpotifyClone.planos 
+WHERE plano_name = 'universitário' 
+OR plano_name = 'familiar'))) b 
+ON b.song_id = a.song_id
+GROUP BY a.song_name
+ORDER BY a.song_name;
