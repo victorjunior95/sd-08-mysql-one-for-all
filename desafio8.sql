af56d7d870de4282ae7499af25cd896b71bdd10b
Bruno Martins <bruno.martins217@gmail.com>
2021-04-30 14:43:38 -0300

    Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,9 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE ON user
     FOR EACH ROW
 BEGIN
-    INSERT INTO user(acao, data_acao)
-    VALUES ('exclusão', NOW());
+    DELETE FROM
+    SpottifyClone.hist_views WHERE user_id = OLD.user_id;
+    DELETE FROM
+    SpotifyClone.following WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
