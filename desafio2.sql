7d0cce5ea369324f8bcbfa12ea9291acf470bba0
antonioarieiro <sfgfffs@gmail.com>
2021-04-30 16:25:08 -0300

    create json file

--- desafio2.sql
+++ desafio2.sql
@@ -1,14 +1,9 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT 
-    (SELECT 
-                COUNT(*)
-            FROM
-                song) AS `cancoes`,
-        (SELECT 
-                COUNT(*)
-            FROM
-                artist) AS `artistas`,
-        (SELECT 
-                COUNT(*)
-            FROM
-                album) AS `albuns`;
+SELECT COUNT(M.idMusica) AS `cancoes`,
+COUNT(DISTINCT AR.idArtista) AS `artistas`,
+COUNT(DISTINCT AB.idAlbum) AS `albuns`
+FROM Musicas M
+INNER JOIN Albuns AB
+ON AB.idAlbum = M.idAlbum
+INNER JOIN Artistas AR
+ON AR.idArtista = AB.idArtista;
