ea2da3ed154bc7be3d526743970c785f012735e9
Bruno Martins <bruno.martins217@gmail.com>
2021-04-29 23:02:59 -0300

    Desafio 6 fix2

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(p.plan_value) AS faturamento_minimo,
+FORMAT(MIN(p.plan_value), 2) AS faturamento_minimo,
 MAX(p.plan_value) AS faturamento_maximo,
 FORMAT(AVG(CEIL(p.plan_value)), 2) AS faturamento_medio,
 FORMAT(SUM(p.plan_value), 2) AS faturamento_total
