d5b6a67e41ea9ce9fbaa17fe6e1ad93329ebbac0
Rita Jeveaux <ritajeveaux@gmail.com>
2021-04-30 20:01:24 -0300

    Requisitos 2-4

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT 
-( SELECT COUNT(*) FROM `SpotifyClone`.`musica`) AS 'cancoes', 
+SELECT
+( SELECT COUNT(*) FROM `SpotifyClone`.`musica`) AS 'cancoes',
 ( SELECT COUNT(*) FROM `SpotifyClone`.`artista`) AS 'artistas',
 ( SELECT COUNT(*) FROM `SpotifyClone`.`album`) AS 'albuns';
 
