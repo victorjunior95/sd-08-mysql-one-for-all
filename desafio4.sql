c6004b36c5c8a32c068f615d8e6cb942dd34ca0c
JRGCast <joao.ralph1@gmail.com>
2021-04-29 23:08:37 -0300

    trocado following por followers no desafio4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,8 @@
 CREATE OR REPLACE VIEW top_3_artistas AS
-    SELECT 
-        a.artist_name AS `artista`, COUNT(f.user_id) AS `seguidores`
-    FROM
-        SpotifyClone.artists AS a
-            INNER JOIN
-        SpotifyClone.following AS f ON f.artist_id = a.artist_id
-    GROUP BY `artista`
-    ORDER BY `seguidores` DESC
-    LIMIT 3;
+SELECT a.artist_name AS `artista`,
+    COUNT(f.user_id) AS `seguidores`
+FROM SpotifyClone.artists AS a
+    INNER JOIN SpotifyClone.followers AS f ON f.artist_id = a.artist_id
+GROUP BY `artista`
+ORDER BY `seguidores` DESC
+LIMIT 3;
