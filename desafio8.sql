9f200df74bff4224fba14b0bfc283e6d87ede860
Fernanda PC <nandateixmarques@gmail.com>
2021-04-30 12:47:23 -0300

    adicionando desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_de_reproducoes
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguidores
+WHERE usuario_id = OLD.usuario_id;
+
+END $$
+DELIMITER ;
