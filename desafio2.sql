3d7b4cbf9339e90413bf63ef2bd5198da27a41a8
MEianelli <filipelouzano@gmail.com>
2021-04-30 11:57:22 -0300

    Requisito 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT ( SELECT COUNT(*) FROM spotifyclone.songs ) AS 'cancoes', 
-( SELECT COUNT(*) FROM spotifyclone.artistas ) AS 'artistas',
-( SELECT COUNT(*) FROM spotifyclone.albuns ) AS 'albuns'
-FROM dual;
+SELECT 
+( SELECT COUNT(*) FROM SpotifyClone.songs ) AS 'cancoes', 
+( SELECT COUNT(*) FROM SpotifyClone.artistas ) AS 'artistas',
+( SELECT COUNT(*) FROM SpotifyClone.albuns ) AS 'albuns';
--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT b.user_name AS 'usuario', c.song_name AS 'nome' 
-FROM spotifyclone.historico_de_reproducoes a
-INNER JOIN spotifyclone.users b ON a.user_id = b.user_id
-INNER JOIN spotifyclone.songs c ON a.song_id = c.song_id
+FROM SpotifyClone.historico_de_reproducoes a
+INNER JOIN SpotifyClone.users b ON a.user_id = b.user_id
+INNER JOIN SpotifyClone.songs c ON a.song_id = c.song_id
 ORDER BY b.user_name, c.song_name;
