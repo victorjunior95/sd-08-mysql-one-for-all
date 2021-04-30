e1fcc4cc7cf226adcc731f1e465447f613dc3385
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 18:32:06 -0300

    fix: not bug

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,13 @@
 USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(usuario_id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE quantidade_de_musicas_historico INT;
     SELECT COUNT(cancao_id)
-    FROM historico_de_reproducoes AS shr
-    WHERE shr.usuario_id = usuario_id INTO quantidade_de_musicas_historico;
+    FROM historico_de_reproducoes
+    WHERE usuario_id = id INTO quantidade_de_musicas_historico;
     RETURN quantidade_de_musicas_historico;
 END $$
 DELIMITER ;
