05923bb9c15e8f1b6cfeb755d8c95bcffea2378e
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-04-30 19:29:53 -0300

    done 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON SpotifyClone.usuario
+    FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.seguindo 
+    WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.historico 
+    WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
