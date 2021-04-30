bde3269601ca326df9a5722385e4c941adfca57d
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-04-29 23:57:23 -0300

    tentando passa no CC

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,12 @@
 CREATE VIEW historico_reproducao_usuarios AS
-    SELECT 
-        nome AS usuario, titulo AS nome
-    FROM
-        SpotifyClone.historico h
-            INNER JOIN
-        SpotifyClone.usuario u ON u.usuario_id = h.usuario_id
-            INNER JOIN
-        SpotifyClone.musica m ON m.musica_id = h.musica_id
-    ORDER BY 1 , 2;
-    
+SELECT 
+nome AS usuario, titulo AS nome
+FROM
+SpotifyClone.historico h
+INNER JOIN
+SpotifyClone.usuario u 
+ON u.usuario_id = h.usuario_id
+INNER JOIN
+SpotifyClone.musica m 
+ON m.musica_id = h.musica_id
+ORDER BY 1 , 2;
