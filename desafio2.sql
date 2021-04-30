085e9cf323adf085ed98c86bed1249f7d433a44f
Blucilha <blucilha.nascimento@gmail.com>
2021-04-30 19:16:05 -0300

    finalmente no desafio 2 correçao parte 1

--- desafio2.sql
+++ desafio2.sql
@@ -3,6 +3,6 @@ USE SpotifyClone;
 CREATE VIEW estatisticas_musicais
 AS
 SELECT COUNT(tss.sings) AS cancoes,
-(SELECT COUNT(ts.stars) FROM table_stars ts) AS artistas,
-(SELECT COUNT(ta.albuns) FROM table_albuns ta) AS albuns
-FROM table_sings tss;
+(SELECT COUNT(ts.stars) FROM SpotifyClone.table_stars ts) AS artistas,
+(SELECT COUNT(ta.albuns) FROM SpotifyClone.table_albuns ta) AS albuns
+FROM SpotifyClone.table_sings tss;
