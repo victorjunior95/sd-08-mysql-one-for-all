116208874b126f79524069b8b72b0904fde74ace
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-04-30 19:45:29 -0300

    issue

--- desafio9.sql
+++ desafio9.sql
@@ -2,11 +2,11 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN artista_nome VARCHAR(100))
 BEGIN
-	SELECT nome AS artista, titulo AS album
-	FROM SpotifyClone.artista a
-	INNER JOIN SpotifyClone.album al
-	ON a.artista_id = al.artista_id
-	WHERE a.nome = artista_nome;
+SELECT nome AS artista, titulo AS album
+FROM SpotifyClone.artista a
+INNER JOIN SpotifyClone.album al
+ON a.artista_id = al.artista_id
+WHERE a.nome = artista_nome;
 END $$
 
 DELIMITER ;
