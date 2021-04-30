6cd827a722bddb13f7fe63db48dcbab661550a50
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-30 18:21:31 -0300

    fix: test two ?

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico_de_reproducoes
+DELETE FROM historico_de_reproducoes
 WHERE usuario_id = OLD.usuario_id;
 DELETE FROM SpotifyClone.seguidores
 WHERE usuario_id = OLD.usuario_id;
