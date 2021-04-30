6e7e85f61316ace4f70e05fc43ef8bc684daa3fa
UmberBn <humberto-bandeira@hotmail.com>
2021-04-29 20:37:02 -0400

    --

--- desafio1.sql
+++ desafio1.sql
@@ -96,16 +96,16 @@ INSERT INTO Albuns(Nome, idArtista)
 
 INSERT INTO Musicas(Nome, idAlbum)
   VALUES
-	("Soul For Us", 1),
+    ("Soul For Us", 1),
     ("Reflections Of Magic", 1),
     ("Dance With Her Own", 1),
-	("Troubles Of My Inner Fire", 2),
+    ("Troubles Of My Inner Fire", 2),
     ("Time Fireworks", 2),
-	("Magic Circus", 3),
+    ("Magic Circus", 3),
     ("Honey, So Do I", 3),
     ("Sweetie, Let's Go Wild", 3),
     ("She Knows", 3),
-	("Fantasy For Me", 4),
+    ("Fantasy For Me", 4),
     ("Celebration Of More", 4),
     ("Rock His Everything", 4),
     ("Home Forever", 4),
