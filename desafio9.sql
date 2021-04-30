a922519b336e497a7eace972692e2566179b6903
Bruno Martins <bruno.martins217@gmail.com>
2021-04-30 16:42:41 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN singer VARCHAR(45))
+BEGIN
+	SELECT 
+    s.singer AS artista,
+    a.album AS album
+    FROM
+    SpotifyClone.singer AS s
+    INNER JOIN SpotifyClone.album AS a
+    ON a.singer_id = s.singer_id
+    WHERE s.singer = singer
+    ORDER BY album;
+END $$;
+DELIMITER ;
