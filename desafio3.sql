f7be6d2c2bbcf6fb86bab9870be178c767641570
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-01 17:46:17 -0300

    c3.4 - fixing code climate delay req3

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT us.usuario_nome usuario, m.musica_nome nome
-FROM SpotifyClone.usuarios us, SpotifyClone.historico h, SpotifyClone.musicas m
+SELECT u.usuario_nome usuario, m.musica_nome nome
+FROM SpotifyClone.usuarios u, SpotifyClone.historico h, SpotifyClone.musicas m
 WHERE 
-us.usuario_id = h.usuario_id AND
+u.usuario_id = h.usuario_id AND
 m.musica_id = h.musica_id
 ORDER BY `usuario`, `nome`;
