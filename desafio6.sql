ba1df25f60892159eabed027121acf4cee99b828
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-05-01 16:31:41 -0300

    Req 06

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 CREATE VIEW faturamento_atual AS
-SELECT 
+    SELECT 
         MIN(p.plan_price) AS `faturamento_minimo`,
         MAX(p.plan_price) AS `faturamento_maximo`,
         ROUND(AVG(p.plan_price), 2) AS `faturamento_medio`,
