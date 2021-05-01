4a03982126fb4eaf27e5eaef3dd23b9457ffe74b
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-01 13:29:14 -0300

    c1.3 - req1 fix evaluator step

--- desafio1.sql
+++ desafio1.sql
@@ -8,12 +8,6 @@ plano_valor DOUBLE,
 PRIMARY KEY(plano_id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE artistas(
-artista_id INT NOT NULL auto_increment,
-artista_nome VARCHAR(50),
-PRIMARY KEY(artista_id)
-) ENGINE = InnoDB;
-
 CREATE TABLE usuarios(
 usuario_id INT NOT NULL auto_increment,
 usuario_nome VARCHAR(50),
@@ -21,6 +15,12 @@ usuario_idade INT,
 plano_id INT,
 PRIMARY KEY(usuario_id),
 FOREIGN KEY(plano_id) REFERENCES planos(plano_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE artistas(
+artista_id INT NOT NULL auto_increment,
+artista_nome VARCHAR(50),
+PRIMARY KEY(artista_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE albuns(
@@ -42,15 +42,15 @@ FOREIGN KEY(album_id) REFERENCES albuns(album_id)
 CREATE TABLE seguidores(
 usuario_id INT NOT NULL,
 artista_id INT NOT NULL,    
-PRIMARY KEY(artista_id, usuario_id),
+PRIMARY KEY(usuario_id, artista_id),
 FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY(artista_id) REFERENCES artistas(artista_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE historico(
-musica_id INT NOT NULL,
 usuario_id INT NOT NULL,
-PRIMARY KEY(musica_id, usuario_id),
+musica_id INT NOT NULL,
+PRIMARY KEY(usuario_id, musica_id),
 FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY(musica_id) REFERENCES musicas(musica_id)
 ) ENGINE = InnoDB;
@@ -79,19 +79,19 @@ VALUES
 ("Honey, Let's Be Silly", 4), ('Thang of Thunder', 5),
 ('Words Of Her Life', 5), ('Without My Streets', 5);
 
-INSERT INTO SpotifyClone.seguidores(artista_id, usuario_id)
+INSERT INTO SpotifyClone.seguidores(usuario_id, artista_id)
 VALUES
-(1,1), (1,2),
-(2,3), (4,4),
-(4,1), (3,1),
-(3,2), (1,3);
+(1,1),(2,1),
+(3,2),(4,4),
+(1,4), (1,3),
+(2,3), (3,1);
 
-INSERT INTO SpotifyClone.historico(musica_id, usuario_id)
+INSERT INTO SpotifyClone.historico(usuario_id, musica_id)
 VALUES
-(1,1), (6,1),
-(14,1), (16,1),
-(2,2), (13,2),
-(15,2), (17,2),
-(4,3), (6,3),
-(16,3), (3,4),
-(11,4), (18,4);
+(1,1), (1,6),
+(1,14), (1,16),
+(2,2), (2,13),
+(2,15), (2,17),
+(3,4), (3,6),
+(3,16), (4,3),
+(4,11), (4,18);
