b591e5cf692a3238076826f3689d180edc3518a5
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-01 20:16:25 -0300

    code climate

--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,8 @@
 CREATE VIEW top_3_artistas AS
-    SELECT 
-        a.artista_nome AS 'artista',
-        COUNT(ua.usuario_id) AS 'seguidores'
-    FROM
-        SpotifyClone.usuario_artista AS ua
-            INNER JOIN
-        SpotifyClone.artistas AS a ON ua.artista_id = a.artista_id
-    GROUP BY a.artista_id
-    ORDER BY `seguidores` DESC , `artista`
-    LIMIT 3;        
+SELECT a.artista_nome AS 'artista', COUNT(ua.usuario_id) AS 'seguidores'
+FROM SpotifyClone.usuario_artista AS ua
+INNER JOIN SpotifyClone.artistas AS a ON ua.artista_id = a.artista_id
+GROUP BY a.artista_id
+ORDER BY `seguidores` DESC , `artista`
+LIMIT 3;
+    
