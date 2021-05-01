a2fc14fafe185b93f0ad01559987ffbb1d8a70b4
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-05-01 15:05:09 -0300

    Req 04

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        a.artist_name AS `artista`,
+        COUNT(f.artist_id) AS `seguidores`
+    FROM
+        SpotifyClone.Artists AS a
+            INNER JOIN
+        SpotifyClone.Followers AS f ON a.artist_id = f.artist_id
+    GROUP BY `artista`
+    ORDER BY `seguidores` DESC
+    LIMIT 3;
