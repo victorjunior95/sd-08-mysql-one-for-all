1f08de805cb22c8036b35ec85b3b478f7e0d15a4
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-01 19:42:44 -0300

    refatora requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
 USE SpotifyClone;
 CREATE VIEW top_3_artistas AS
     SELECT 
-        a.artista_nome AS `artista`,
-        COUNT(ua.artista_id) AS `seguidores`
+        a.artista_nome AS artista,
+        COUNT(ua.artista_id) AS seguidores
     FROM
         SpotifyClone.artistas AS a
             INNER JOIN
         SpotifyClone.usuario_artista AS ua ON a.artista_id = ua.artista_id
     GROUP BY ua.artista_id
-    ORDER BY `seguidores` DESC , `artista`
+    ORDER BY seguidores DESC , artista
     LIMIT 3;
