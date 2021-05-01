84e868d38103f6d19471cbbb31c332f5526434bf
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-01 19:10:46 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        a.artista_nome AS `artista`,
+        COUNT(ua.artista_id) AS `seguidores`
+    FROM
+        SpotifyClone.artistas AS a
+            INNER JOIN
+        SpotifyClone.usuario_artista AS ua ON a.artista_id = ua.artista_id
+    GROUP BY ua.artista_id
+    ORDER BY `seguidores` DESC , `artista`
+    LIMIT 3;
