ea8dce779ccdf3bea0e1ec94a6a917a731ec92ef
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-01 19:23:16 -0300

    refatora desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -75,14 +75,14 @@ INSERT INTO SpotifyClone.albuns(album_nome, artista_id)
 VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
 
 INSERT INTO SpotifyClone.cancoes(cancao_nome, album_id) VALUES 
-('Soul for Us', 1), ('Reflections Of Magic', 1),
+('Soul For Us', 1), ('Reflections Of Magic', 1),
 ('Dance With Her Own', 1), ('Troubles Of My Inner Fire', 2),
 ('Time Fireworks', 2), ('Magic Circus', 3),
 ('Honey, So Do I', 3), ("Sweetie, Let's Go Wild", 3),
 ('She Knows', 3), ('Fantasy For Me', 4),
 ('Celebration Of More', 4), ('Rock His Everything', 4),
 ('Home Forever', 4), ('Diamond Power', 4),
-("Honey, Let's Be Silly", 4), ('Thang of Thunder', 5),
+("Honey, Let's Be Silly", 4), ('Thang Of Thunder', 5),
 ('Words Of Her Life', 5), ('Without My Streets', 5);
 
 INSERT INTO SpotifyClone.usuario_artista(usuario_id, artista_id) VALUES 
