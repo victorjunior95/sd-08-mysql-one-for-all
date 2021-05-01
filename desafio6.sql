0c3dc77e38c4a6627eb8df26539e6977caecaac4
hugobrr <hugobrr@gmail.com>
2021-05-01 12:07:06 -0300

    Resolvendo os 4 erros do cc

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,6 @@
 CREATE VIEW faturamento_atual AS
- SELECT
-  ( SELECT ROUND(MIN(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_minimo', 
-  ( SELECT ROUND(MAX(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_maximo', 
-  ( SELECT ROUND(AVG(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_medio', 
+  SELECT
+  ( SELECT ROUND(MIN(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_minimo',
+  ( SELECT ROUND(MAX(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_maximo',
+  ( SELECT ROUND(AVG(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_medio',
   ( SELECT ROUND(SUM(valorPlano),2) FROM SpotifyClone.Planos )  AS 'faturamento_total';
- 
- 
