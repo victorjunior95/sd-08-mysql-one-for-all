c4f866697f0cd07145151b2a2ec8664aea95ac00
Uilton <itoclick@gmail.com>
2021-05-02 05:09:20 -0300

    req3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+USE SpotifyClone ;
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.usuario AS usuario, m.cancoes AS nome FROM SpotifyClone._usuarios AS u
+INNER JOIN SpotifyClone._historico AS h ON  u.usuario_id = h.usuario_id
+INNER JOIN SpotifyClone._musicas AS m ON m.cancoes_id = h.cancoes_id
+ORDER BY usuario;
