15a7d2d9bad9170778f2177f6f1e54933037519d
caiomzrocha <caiomzrocha@gmail.com>
2021-05-02 16:00:32 -0300

    [REQ-03] Exibe o histórico de reprodução para cada pessoa usuária.

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+CREATE VIEW historico_reproducao_usuarios AS
+  SELECT
+    users.usuario AS usuario,
+    songs.cancao AS nome
+  FROM SpotifyClone.historic AS historic
+    INNER JOIN SpotifyClone.users AS users
+      ON historic.usuario_id = users.usuario_id
+	INNER JOIN SpotifyClone.songs AS songs
+      ON historic.cancao_id = songs.cancao_id
+  ORDER BY `usuario`, `nome`;
