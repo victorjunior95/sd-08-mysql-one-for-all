cace3f8fad91e3a3fb6ff64ab856bd16a3e79062
caiomzrocha <caiomzrocha@gmail.com>
2021-05-02 17:46:36 -0300

    [REQ-06][FIX] Correção ortográfica.

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
   SELECT
-    MIN(valor_plano) AS faturamento_minino,
+    MIN(valor_plano) AS faturamento_minimo,
     MAX(valor_plano) AS faturamento_maximo,
     ROUND(AVG(valor_plano), 2) AS faturamento_medio,
     SUM(valor_plano) AS faturamento_total
