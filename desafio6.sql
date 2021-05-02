a0956c954bd0e344b404565abbdbe917af776429
caiomzrocha <caiomzrocha@gmail.com>
2021-05-02 17:33:31 -0300

    [REQ-06] Exibe o relatório de faturamento da empresa.

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+CREATE VIEW faturamento_atual AS
+  SELECT
+    MIN(valor_plano) AS faturamento_minino,
+    MAX(valor_plano) AS faruramento_maximo,
+    ROUND(AVG(valor_plano), 2) AS faturamento_medio,
+    SUM(valor_plano) AS faturamento_total
+  FROM
+    (SELECT plans.valor_plano
+      FROM SpotifyClone.users AS users
+    INNER JOIN SpotifyClone.plans AS plans
+      ON users.plano_id = plans.plano_id)
+  AS planos;
