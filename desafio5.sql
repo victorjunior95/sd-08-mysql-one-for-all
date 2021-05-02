93201b4b083a26bc3ae571bacb02e01c2af216c7
caiomzrocha <caiomzrocha@gmail.com>
2021-05-02 17:10:44 -0300

    [REQ-05] Exibe top 2 hits mais tocados no momento.

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_2_hits_no_momento AS
+  SELECT
+    songs.cancao AS cancao,
+    COUNT(historic.cancao_id) AS reproducoes
+    FROM SpotifyClone.songs AS songs
+    INNER JOIN SpotifyClone.historic AS historic
+      ON songs.cancao_id = historic.cancao_id
+  GROUP BY cancao
+  ORDER BY reproducoes DESC, cancao ASC
+  LIMIT 2;
