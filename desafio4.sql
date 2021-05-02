94a06f89d0fec84ae7c97564acec7ab5c913cd68
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-01 22:27:33 -0300

    desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -3,6 +3,6 @@ SELECT a.artista_nome AS 'artista',
   COUNT(ua.usuario_id) AS 'seguidores'
 FROM SpotifyClone.usuario_artista AS ua
   INNER JOIN SpotifyClone.artistas AS a ON ua.artista_id = a.artista_id
-GROUP BY a.artista_id
+GROUP BY ua.artista_id
 ORDER BY `seguidores` DESC, `artista`
 LIMIT 3;
