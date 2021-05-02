2996055a554f7609f9835b2150d352191ad1c6d5
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-02 10:02:37 -0300

    c4.1 - fixing schema

--- desafio1.sql
+++ desafio1.sql
@@ -56,7 +56,7 @@ FOREIGN KEY(musica_id) REFERENCES musicas(musica_id)
 ) ENGINE = InnoDB;
 
 INSERT INTO SpotifyClone.artistas(artista_nome)
-VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freddie Shannon');
+VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 
 INSERT INTO SpotifyClone.planos(plano_nome, plano_valor)
 VALUES ('gratuito', 0.00), ('familiar', 7.99), ('universitário', 5.99);
@@ -69,14 +69,14 @@ VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent',
 
 INSERT INTO SpotifyClone.musicas(musica_nome, album_id)
 VALUES 
-('Soul for Us', 1), ('Reflections Of Magic', 1),
+('Soul For Us', 1), ('Reflections Of Magic', 1),
 ('Dance With Her Own', 1), ('Troubles Of My Inner Fire', 2),
 ('Time Fireworks', 2), ('Magic Circus', 3),
 ('Honey, So Do I', 3), ("Sweetie, Let's Go Wild", 3),
 ('She Knows', 3), ('Fantasy For Me', 4),
 ('Celebration Of More', 4), ('Rock His Everything', 4),
 ('Home Forever', 4), ('Diamond Power', 4),
-("Honey, Let's Be Silly", 4), ('Thang of Thunder', 5),
+("Honey, Let's Be Silly", 4), ('Thang Of Thunder', 5),
 ('Words Of Her Life', 5), ('Without My Streets', 5);
 
 INSERT INTO SpotifyClone.seguidores(usuario_id, artista_id)
