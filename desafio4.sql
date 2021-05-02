aec51ccdbeb9c1001e77044a388b248924eb9a5f
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-02 10:10:29 -0300

    c4.2 - adding LIMIT

--- desafio4.sql
+++ desafio4.sql
@@ -4,4 +4,5 @@ FROM SpotifyClone.artistas a
 INNER JOIN SpotifyClone.seguidores s
 ON a.artista_id = s.artista_id
 GROUP BY s.artista_id
-ORDER BY `seguidores` DESC, `artista`;
+ORDER BY `seguidores` DESC, `artista`
+LIMIT 3;
