9e0d42fba83b0f53dff1644e2753e70cc8723e27
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-01 23:33:16 -0300

    desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 CREATE VIEW top_3_artistas AS
 SELECT a.artista_nome AS artista,
   COUNT(ua.usuario_id) AS seguidores
-FROM SpotifyClone.usuario_artista AS ua
-  INNER JOIN SpotifyClone.artistas AS a ON ua.artista_id = a.artista_id
+FROM usuario_artista AS ua
+  INNER JOIN artistas AS a ON ua.artista_id = a.artista_id
 GROUP BY ua.artista_id
-ORDER BY seguidores DESC, artista
+ORDER BY seguidores DESC, artista ASC
 LIMIT 3;
