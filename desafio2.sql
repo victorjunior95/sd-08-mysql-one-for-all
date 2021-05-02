701f29e06cae3147f77fb8e309d56258961a4b29
lucasbgama <lucasbg@protonmail.com>
2021-05-02 02:52:44 -0300

    fix use statement

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,4 @@
-USE SpotifyClone;
-CREATE VIEW estatisticas_musicais AS
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
 SELECT (SELECT COUNT(cancao_id) FROM cancoes) AS cancoes,
 (SELECT COUNT(artista_id) FROM artistas) AS artistas,
 (SELECT COUNT(album_id) FROM albuns) AS albuns;
