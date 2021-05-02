e808cbe377c716c352791c53084a6d4b35a2b52e
caiomzrocha <caiomzrocha@gmail.com>
2021-05-02 16:24:08 -0300

    [REQ-03][FIX] Substitui os tabs por espaços.

--- desafio3.sql
+++ desafio3.sql
@@ -5,6 +5,6 @@ CREATE VIEW historico_reproducao_usuarios AS
   FROM SpotifyClone.historic AS historic
     INNER JOIN SpotifyClone.users AS users
       ON historic.usuario_id = users.usuario_id
-	INNER JOIN SpotifyClone.songs AS songs
+    INNER JOIN SpotifyClone.songs AS songs
       ON historic.cancao_id = songs.cancao_id
   ORDER BY `usuario`, `nome`;
