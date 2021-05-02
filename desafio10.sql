904d4cbca1a3f9766360c33a049e4d7bd7c7a661
caiomzrocha <caiomzrocha@gmail.com>
2021-05-02 20:38:28 -0300

    [REQ-10][FIX] Altera a entrada da funcao quantidade_musica_no_historico para receber o id.

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,11 @@
 USE SpotifyClone;
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(usuario VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE total_cancoes INT;
   SELECT COUNT(*) FROM historic
-  INNER JOIN users
-  ON historic.usuario_id = users.usuario_id
-  WHERE users.usuario = usuario
+  WHERE usuario_id = id
   INTO total_cancoes;
   RETURN total_cancoes;
 END $$
