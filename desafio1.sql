df3af9b7690ef79ec99b1ed56ae920145b766c61
Alana Monteiro <lanaah.15@gmail.com>
2021-05-02 15:00:20 -0300

    desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,122 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+CREATE TABLE SpotifyClone.plans(
+plan_id INT PRIMARY KEY AUTO_INCREMENT,
+plan VARCHAR(50) NOT NULL,
+plan_value DOUBLE NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.users(
+user_id INT PRIMARY KEY AUTO_INCREMENT,
+user_name VARCHAR(50) NOT NULL,
+age DOUBLE NOT NULL,
+plan_id INT NOT NULL,
+FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.artists(
+artist_id INT PRIMARY KEY AUTO_INCREMENT,
+artist VARCHAR(50)
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.albums(
+album_id INT PRIMARY KEY AUTO_INCREMENT,
+album VARCHAR(50),
+artist_id INT NOT NULL,
+FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.songs(
+song_id INT PRIMARY KEY AUTO_INCREMENT,
+song VARCHAR(50),
+album_id INT NOT NULL,
+FOREIGN KEY (album_id) REFERENCES albums(album_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.history(
+user_id INT NOT NULL,
+song_id INT NOT NULL,
+PRIMARY KEY (user_id, song_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY (song_id) REFERENCES songs(song_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.following_artists(
+user_id INT NOT NULL,
+artist_id INT NOT NULL,
+PRIMARY KEY (user_id, artist_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+) ENGINE = InnoDB;
+
+INSERT INTO SpotifyClone.plans (plan, plan_value)
+VALUES ('gratuito', 1),
+('familiar', 2),
+('universitário', 3);
+
+INSERT INTO SpotifyClone.users (user_name, age, plan_id)
+VALUES ('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO SpotifyClone.artists (artist)
+VALUES ('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freddie Shannon');
+
+INSERT INTO SpotifyClone.albums (album, artist_id)
+VALUES ('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO SpotifyClone.songs (song, album_id)
+VALUES ('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+("She Knows", 3),
+("Fantasy For Me", 4),
+("Celebration Of More", 4),
+("Rock His Everything", 4),
+("Home Forever", 4),
+("Diamond Power", 4),
+("Honey, Let's Be Silly", 4),
+("Thang Of Thunder", 5),
+("Words Of Her Life", 5),
+("Without My Streets", 5);
+
+INSERT INTO SpotifyClone.history (user_id, song_id)
+VALUES (1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 16),
+(4, 11);
+
+INSERT INTO SpotifyClone.following_artists (user_id, artist_id)
+VALUES (1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
