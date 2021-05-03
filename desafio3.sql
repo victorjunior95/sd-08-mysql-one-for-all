7371d7fc49ab4e014bf7a3ffc95b5bdb7e490258
MarceloBorges <melombm@hotmail.com>
2021-05-03 08:00:19 -0500

    requisito 3 deletando o arquivo diagrama

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+CREATE VIEW historico_reproducao_usuarios AS 
+	SELECT
+		us.nome_usuario AS usuario,
+        cc.nome_cancao AS cancoes
+	FROM
+		SpotifyClone.usuarioTB AS us
+			INNER JOIN
+		SpotifyClone.cancoesTB AS cc
+        ORDER BY usuario, cancoes;
+        
+ -- SELECT * FROM historico_reproducao_usuarios;
+ 
