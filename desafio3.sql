0771eee373b0d9fb350e9940063c1b3727458ae2
MarceloBorges <melombm@hotmail.com>
2021-05-03 11:03:55 -0500

    correção do cancao por nome

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS 
     SELECT
       us.nome_usuario AS usuario,
-      cc.nome_cancao AS cancoes
+      cc.nome_cancao AS nome
     FROM
       SpotifyClone.usuarioTB AS us INNER JOIN
       SpotifyClone.historicoTB AS his ON his.usuarioID = us.usuarioID INNER JOIN
       SpotifyClone.cancoesTB AS cc ON cc.cancaoID = his.cancaoID
-ORDER BY usuario, cancoes;   
+ORDER BY usuario, nome;   
 -- SELECT * FROM historico_reproducao_usuarios;
