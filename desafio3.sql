65c7a7e8b965ee9a9cf609f9bba21c753e3e3355
Daniel <marcio.daniel@msn.com>
2021-05-03 18:02:44 -0300

    3- Requisito

---
+++ desafio3.sql
@@ -0,0 +1,14 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+    u.nome_usuario AS usuario,
+    c.nome_cancao AS nome
+FROM
+    usuario AS u,
+    historico_reproducao As hr,
+    cancao As c
+WHERE
+    u.id_usuario = hr.id_usuario
+    AND c.id_cancao = hr.id_cancao
+ORDER BY
+    `usuario`,
+    `nome`;
