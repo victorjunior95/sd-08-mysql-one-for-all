9ed0980b4f5404acaa28b9fadde6738772f9eab6
Uilton Rocha <itoclick@gmail.com>
2021-05-03 02:03:23 -0300

    req4

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,3 @@
-USE SpotifyClone ;
-CREATE VIEW 'estatisticas_musicais' AS
-SELECT MAX(m.cancoes_id) AS cancoes, MAX(a.artista_id) AS artistas, MAX(al.album_id) AS  albuns
-FROM SpotifyClone._musicas as m, SpotifyClone._artistas as a, SpotifyClone._albums AS al;
+CREATE VIEW SpotifyClone.estatisticas_musicais AS 
+SELECT MAX(m.cancoes_id) AS cancoes, MAX(a.artista_id) AS artistas, MAX(l.album_id) AS  albuns
+FROM SpotifyClone._musicas as m, SpotifyClone._artistas AS a, SpotifyClone._albums AS l;
--- desafio4.sql
+++ desafio4.sql
@@ -1,19 +1,6 @@
-USE SpotifyClone ;
-CREATE VIEW top_3_artistas AS
-SELECT m.artista AS artistas, COUNT(u.usuario_id) AS seguidores
-FROM SpotifyClone._artistas AS m, SpotifyClone._usuarios AS u
-GROUP BY artistas;
-
-
-
-SELECT m.artista AS artistas, COUNT(u.usuario_id) AS seguidores
-FROM SpotifyClone._artistas AS m, SpotifyClone._usuarios AS u
-GROUP BY artistas;
--- era para vir o dado de _seguindo
-
-USE SpotifyClone ;
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.usuario AS usuario, m.cancoes AS nome FROM SpotifyClone._usuarios AS u
-INNER JOIN SpotifyClone._historico AS h ON  u.usuario_id = h.usuario_id
-INNER JOIN SpotifyClone._musicas AS m ON m.cancoes_id = h.cancoes_id
-ORDER BY usuario;
+CREATE VIEW SpotifyClone.top_3_artistas AS
+SELECT a.artista AS artista, count(u.artista_id) AS seguidores
+FROM SpotifyClone._artistas AS a
+inner join SpotifyClone._seguindo AS u on a.artista_id = u.artista_id
+GROUP BY artista
+ORDER BY seguidores DESC, artista LIMIT 3;
