67fd4eda85f7baecffdfa57e80c275ab94426bfc
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-03 00:22:15 -0300

    C7 - 8th Req Sent

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.user_follows
+WHERE user_id = OLD.id
+DELETE FROM SpotifyClone.histories
+WHERE user_id = OLD.id
+END
+DELIMITER;
