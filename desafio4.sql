73699ea1d0ac001ed17a21c3c520f7acd063e86d
Gustavo Fernandes Thirion <65987099+Gustaft86@users.noreply.github.com>
2021-05-03 11:34:05 -0300

    Delete desafio4.sql
    codeclimate error

--- desafio4.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW top_3_artistas AS
-SELECT seguindo_artistas AS `artista`, COUNT(*) AS `seguidores`
-FROM SpotifyClone.Seguidos AS a
-GROUP BY `seguindo_artistas`
-ORDER BY `seguidores` DESC, `artista` ASC
-LIMIT 3;
