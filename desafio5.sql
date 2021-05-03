eedd16657d1a0ed82b1d3ce6b4d982c53faaf6a2
Blucilha <blucilha.nascimento@gmail.com>
2021-05-03 13:45:35 -0300

    finalmente no desafio 5 correçao

--- desafio5.sql
+++ desafio5.sql
@@ -3,10 +3,10 @@ top_2_hits_do_momento
 AS
 SELECT
 (SELECT sings FROM SpotifyClone.table_sings ts
-  WHERE ts.id_sings = th.id_sings) AS cancoes,
+  WHERE ts.id_sings = th.id_sings) AS cancao,
 COUNT((SELECT users FROM SpotifyClone.table_users tu
   WHERE tu.id_users = th.id_users)) AS reproducoes
 FROM SpotifyClone.table_history th
 GROUP BY cancoes
-ORDER BY reproducoes DESC, cancoes
+ORDER BY reproducoes DESC, cancao
 LIMIT 2;
