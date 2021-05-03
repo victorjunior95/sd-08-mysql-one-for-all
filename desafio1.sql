f2f50e3c81e3d29a30230da1679ba65c4c8ab1c8
Daniel <marcio.daniel@msn.com>
2021-05-03 16:07:53 -0300

    Fix 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,82 +1,82 @@
 DROP DATABASE IF EXISTS `SpotifyClone`;
 
-CREATE SCHEMA IF NOT EXISTS `SpotifyClone`;
+CREATE DATABASE IF NOT EXISTS `SpotifyClone`;
 
 USE `SpotifyClone`;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`plano`;
+DROP TABLE IF EXISTS `plano`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`plano` (
+CREATE TABLE IF NOT EXISTS `plano` (
     `id_plano` INT NOT NULL AUTO_INCREMENT,
     `nome_plano` VARCHAR(45) NOT NULL,
     `valor_plano` DECIMAL(3, 2) NOT NULL,
     PRIMARY KEY (`id_plano`)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`usuario`;
+DROP TABLE IF EXISTS `usuario`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuario` (
+CREATE TABLE IF NOT EXISTS `usuario` (
     `id_usuario` INT NOT NULL AUTO_INCREMENT,
     `nome_usuario` VARCHAR(45) NOT NULL,
     `idade_usuario` INT NOT NULL,
     `id_plano` INT NOT NULL,
     PRIMARY KEY (`id_usuario`),
     INDEX `fk_usuario_plano1_idx` (`id_plano` ASC) VISIBLE,
-    CONSTRAINT `fk_usuario_plano1` FOREIGN KEY (`id_plano`) REFERENCES `SpotifyClone`.`plano` (`id_plano`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT `fk_usuario_plano1` FOREIGN KEY (`id_plano`) REFERENCES `plano` (`id_plano`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`artista`;
+DROP TABLE IF EXISTS `artista`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artista` (
+CREATE TABLE IF NOT EXISTS `artista` (
     `id_artista` INT NOT NULL AUTO_INCREMENT,
     `nome_artista` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id_artista`)
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`album`;
+DROP TABLE IF EXISTS `album`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`album` (
+CREATE TABLE IF NOT EXISTS `album` (
     `id_album` INT NOT NULL AUTO_INCREMENT,
     `nome_album` VARCHAR(45) NOT NULL,
     `id_artista` INT NOT NULL,
     PRIMARY KEY (`id_album`),
     INDEX `fk_albuns_artistas1_idx` (`id_artista` ASC) VISIBLE,
-    CONSTRAINT `fk_album_artista1` FOREIGN KEY (`id_artista`) REFERENCES `SpotifyClone`.`artista` (`id_artista`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT `fk_album_artista1` FOREIGN KEY (`id_artista`) REFERENCES `artista` (`id_artista`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`cancao`;
+DROP TABLE IF EXISTS `cancao`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`cancao` (
+CREATE TABLE IF NOT EXISTS `cancao` (
     `id_cancao` INT NOT NULL AUTO_INCREMENT,
     `nome_cancao` VARCHAR(45) NOT NULL,
     `id_album` INT NOT NULL,
     PRIMARY KEY (`id_cancao`),
     INDEX `fk_cancao_album1_idx` (`id_album` ASC) VISIBLE,
-    CONSTRAINT `fk_cancao_album1` FOREIGN KEY (`id_album`) REFERENCES `SpotifyClone`.`album` (`id_album`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT `fk_cancao_album1` FOREIGN KEY (`id_album`) REFERENCES `album` (`id_album`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`historico_reproducao`;
+DROP TABLE IF EXISTS `historico_reproducao`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`historico_reproducao` (
+CREATE TABLE IF NOT EXISTS `historico_reproducao` (
     `id_cancao` INT NOT NULL,
     `id_usuario` INT NOT NULL,
     PRIMARY KEY (`id_cancao`, `id_usuario`),
     INDEX `fk_historico_reproducao_cancao1_idx` (`id_cancao` ASC) VISIBLE,
     INDEX `fk_historico_reproducao_usuario1_idx` (`id_usuario` ASC) VISIBLE,
-    CONSTRAINT `fk_historico_reproducao_cancao1` FOREIGN KEY (`id_cancao`) REFERENCES `SpotifyClone`.`cancao` (`id_cancao`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `fk_historico_reproducao_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `SpotifyClone`.`usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT `fk_historico_reproducao_cancao1` FOREIGN KEY (`id_cancao`) REFERENCES `cancao` (`id_cancao`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_historico_reproducao_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotifyClone`.`usuario_artista`;
+DROP TABLE IF EXISTS `usuario_artista`;
 
-CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuario_artista` (
+CREATE TABLE IF NOT EXISTS `usuario_artista` (
     `id_usuario` INT NOT NULL,
     `id_artista` INT NOT NULL,
     PRIMARY KEY (`id_artista`, `id_usuario`),
     INDEX `fk_usuario_artista_usuario1_idx` (`id_usuario` ASC) VISIBLE,
     INDEX `fk_usuario_artista_artista1_idx` (`id_artista` ASC) VISIBLE,
-    CONSTRAINT `fk_usuario_artista_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `SpotifyClone`.`usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `fk_usuario_artista_artista1` FOREIGN KEY (`id_artista`) REFERENCES `SpotifyClone`.`artista` (`id_artista`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT `fk_usuario_artista_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_usuario_artista_artista1` FOREIGN KEY (`id_artista`) REFERENCES `artista` (`id_artista`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
 INSERT INTO
