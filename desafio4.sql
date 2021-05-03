40ea6ba2a79ef6b7305da3927609987b9e7e3df9
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-02 22:27:37 -0300

    C3 - FIX - 4th Req Fixed

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_3_artistas AS
-	SELECT a.name AS artista, count(f.artist_id) AS seguidores
-    FROM spotifyclone.artists AS a
-    INNER JOIN spotifyclone.user_follows AS f ON a.id = f.artist_id
-    GROUP BY a.name
-    ORDER BY count(f.artist_id) DESC, a.name
-    LIMIT 3;
+SELECT a.name AS artista, count(f.artist_id) AS seguidores
+FROM spotifyclone.artists AS a
+INNER JOIN spotifyclone.user_follows AS f ON a.id = f.artist_id
+GROUP BY a.name
+ORDER BY count(f.artist_id) DESC, a.name
+LIMIT 3;
