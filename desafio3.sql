28c8de0fb9169964ef2ac9e1e3d6e416d4bbf733
Luiz Reina <luizreeina@gmail.com>
2021-05-03 14:25:26 -0300

    correcao de sintaxe no desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 CREATE VIEW historico_reproducao_usuarios AS
-  SELECT Usuario.Nome AS usuario, Cancao.Cancao as nome FROM Usuario
+  SELECT Usuario.Nome AS usuario, Cancao.Nome as nome FROM Usuario
   INNER JOIN Historico_Cancao history
   ON history.Usuario_id = Usuario.Usuario_id
   INNER JOIN Cancao c
