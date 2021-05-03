1cf3200a370874c7a8f345f5ad85bc7549a38cd6
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-03 16:46:24 -0300

    Requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+DROP VIEW IF EXISTS SpotifyClone.estatisticas_musicais;
+
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+SELECT
+  (SELECT COUNT(*) FROM SpotifyClone.musicas) AS cancoes,
+  (SELECT COUNT(*) FROM SpotifyClone.artistas) AS artistas,
+  (SELECT COUNT(*) FROM SpotifyClone.albuns) AS albuns;
