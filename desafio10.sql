581999258dc7afc92c1186f2401593901b2b2668
hugobrr <hugobrr@gmail.com>
2021-05-03 12:27:50 -0300

    VENdo o  erro do req 7

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE quantidade_musicas_no_historico(in nameOfUser varchar(50),  OUT numberOfHistoric NUMERIC)
+BEGIN
+	SELECT COUNT(usuario_id)  FROM Historico AS h
+--     -- WHERE al.artista_id = ar.artista_id
+    INNER JOIN Usuarios AS u
+    ON u.usuarioName = nameOfUser AND  h.usuario_id = u.usuario_id
+    INTO numberOfHistoric;
+--     GROUP BY al.album
+--     ORDER BY 2;
+END $$
+DELIMITER ;
+CALL quantidade_musicas_no_historico("Bill");
