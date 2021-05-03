023d0bf264a7a5fdf7cda083edb94ef12d7cf49f
MarceloBorges <melombm@hotmail.com>
2021-05-03 10:48:14 -0500

    requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $M
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarioTB
+  FOR EACH ROW
+BEGIN
+  DELETE FROM seguidoresTB WHERE usuarioID = OLD.usuarioID;
+  DELETE FROM historicoTB WHERE usuarioID = OLD.usuarioID;
+END
+$M DELIMITER ;
