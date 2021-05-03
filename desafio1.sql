108468bb8889086f3fffcc3d1eb62d268f6e91eb
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-03 13:28:52 -0300

    Create 'albuns' table in SpotifyClone database

--- desafio1.sql
+++ desafio1.sql
@@ -5,7 +5,7 @@ USE SpotifyClone;
 CREATE TABLE planos (
   `id_plano` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `plano` VARCHAR(25),
-  `valor_plano` DECIMAL(4, 2),
+  `valor_plano` DECIMAL(4, 2)
 ) ENGINE = InnoDB;
 
 CREATE TABLE historicos (
@@ -15,13 +15,23 @@ CREATE TABLE historicos (
   FOREIGN KEY (`id_usuario`)
     REFERENCES `usuarios` (`id_usuario`),
   FOREIGN KEY (`id_cancao`)
-    REFERENCES `cancoes` (`id_cancao`),
+    REFERENCES `cancoes` (`id_cancao`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE artistas (
   `id_artista` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `artista` VARCHAR(100) NOT NULL UNIQUE,
 ) ENGINE = InnoDB;
+
+CREATE TABLE albuns(
+  `id_album` INT NOT NULL AUTO_INCREMENT,
+  `album` VARCHAR(100) NOT NULL,
+  `id_artista` INT NOT NULL,
+  FOREIGN KEY (`id_artista`)
+    REFERENCES `artistas` (`id_artista`)
+) ENGINE = InnoDB;
+
+CREATE TABLE cancoes() ENGINE = InnoDB;
 
 CREATE TABLE usuarios (
   `id_usuario` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -30,4 +40,4 @@ CREATE TABLE usuarios (
   `id_plano` INT NOT NULL,
   FOREIGN KEY (`id_plano`)
     REFERENCES `planos` (`id_plano`)
-)
+) ENGINE = InnoDB;
