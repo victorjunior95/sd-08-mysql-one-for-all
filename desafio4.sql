c439e3a45057c92592f20e068109b2a8aa8d2a85
Blucilha <blucilha.nascimento@gmail.com>
2021-05-03 12:27:16 -0300

    finalmente no desafio 4 correçao

--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ top_3_artistas
 AS
 SELECT
 (SELECT stars FROM SpotifyClone.table_stars ts
-  WHERE ts.id_stars = tf.id_stars) AS artistas,
+  WHERE ts.id_stars = tf.id_stars) AS artista,
 COUNT((SELECT users FROM SpotifyClone.table_users tu
   WHERE tu.id_users = tf.id_users)) AS seguidores
 FROM SpotifyClone.table_follow_stars tf
