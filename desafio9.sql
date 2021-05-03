053f0049ecfd5f5493b1f730f48fd49e37822b93
hugobrr <hugobrr@gmail.com>
2021-05-03 12:13:22 -0300

    Corrigindo os erros do Desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,7 +2,7 @@ USE SpotifyClone;
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(in targetArtistName varchar(50))
 BEGIN
-	SELECT targetArtistName AS 'artista', al.album AS 'album' FROM Albums AS al
+    SELECT targetArtistName AS 'artista', al.album AS 'album' FROM Albums AS al
     -- WHERE al.artista_id = ar.artista_id
     INNER JOIN Artistas AS ar
     ON ar.artistaName = targetArtistName AND  al.artista_id = ar.artista_id
