5dba62c8e7550a0fd09eb0e608a8292a228a566a
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-05-03 00:04:18 -0300

    C6 - FIX - 7th Req Fixed

--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,5 @@ SELECT a.name AS artista, l.name AS album, count(f.artist_id) AS seguidores
 FROM SpotifyClone.artists AS a
 INNER JOIN SpotifyClone.albuns AS l ON a.id = l.artist_id
 INNER JOIN SpotifyClone.user_follows AS f ON a.id = f.artist_id
-GROUP BY l.name
+GROUP BY l.name, a.name
 ORDER BY count(f.artist_id) DESC, a.name, l.name;
