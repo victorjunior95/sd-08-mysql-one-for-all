478371b667cbfd4e6fb05f81ec8108a8313e5e9c
hugobrr <hugobrr@gmail.com>
2021-05-03 12:11:25 -0300

    Requisito 7, 9 e 11  passando

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,15 @@
 -- https://stackoverflow.com/questions/5528854/usage-of-mysqls-if-exists
 -- https://stackoverflow.com/questions/8130110/mysql-avg-on-conditional
--- CREATE VIEW cancoes_premium AS
--- SELECT 
--- sub.`nome` AS 'nome',
--- sub.`reproducoes` AS 'reproducoes'
--- FROM(
--- SELECT   m.musicaName AS 'nome', COUNT(h.musica_id) AS 'reproducoes' FROM  SpotifyClone.Musicas AS m
--- INNER JOIN  SpotifyClone.Usuarios AS  u
--- ON u.plano_id IN (2,3)
--- INNER JOIN  SpotifyClone.Historico AS  h
--- ON h.musica_id = m.musica_id
--- GROUP BY m.musicaName
--- ) AS sub 
--- ORDER BY 1;
+CREATE VIEW cancoes_premium AS
+SELECT 
+sub.`nome` AS 'nome',
+sub.`reproducoes` AS 'reproducoes'
+FROM(
+SELECT   m.musicaName AS 'nome', COUNT(h.musica_id) AS 'reproducoes' FROM  SpotifyClone.Musicas AS m
+INNER JOIN  SpotifyClone.Usuarios AS  u 
+ON u.plano_id IN (2,3)
+INNER JOIN  SpotifyClone.Historico AS  h
+ON h.musica_id = m.musica_id AND h.usuario_id = u.usuario_id
+GROUP BY m.musicaName
+) AS sub 
+ORDER BY 1;
---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(in targetArtistName varchar(50))
+BEGIN
+	SELECT targetArtistName AS 'artista', al.album AS 'album' FROM Albums AS al
+    -- WHERE al.artista_id = ar.artista_id
+    INNER JOIN Artistas AS ar
+    ON ar.artistaName = targetArtistName AND  al.artista_id = ar.artista_id
+    GROUP BY al.album
+    ORDER BY 2;
+END $$
+DELIMITER ;
+CALL albuns_do_artista('Walter Phoenix');
