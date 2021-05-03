37e0ac3db97591cafc0b07bdcc6998724708684a
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-03 13:48:45 -0300

    buiding

--- desafio1.sql
+++ desafio1.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
 USE SpotifyClone;
 
+-- Planos
+
 CREATE TABLE planos (
   `id_plano` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `plano` VARCHAR(25),
@@ -14,6 +16,8 @@ VALUES
 ('familiar', 7.99),
 ('universitario', 5.99);
 
+-- Historicos
+
 CREATE TABLE historicos (
   `id_usuario` INT NOT NULL,
   `id_cancao` INT NOT NULL,
@@ -24,10 +28,38 @@ CREATE TABLE historicos (
     REFERENCES `cancoes` (`id_cancao`)
 ) ENGINE = InnoDB;
 
+INSERT INTO historicos (id_usuario, id_cancao)
+VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
+
+-- Artistas
+
 CREATE TABLE artistas (
   `id_artista` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `artista` VARCHAR(100) NOT NULL UNIQUE,
 ) ENGINE = InnoDB;
+
+INSERT INTO artistas (artista)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+-- Albuns
 
 CREATE TABLE albuns(
   `id_album` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -37,6 +69,16 @@ CREATE TABLE albuns(
     REFERENCES `artistas` (`id_artista`)
 ) ENGINE = InnoDB;
 
+INSERT INTO albuns (album, id_artista)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+-- Cancoes
+
 CREATE TABLE cancoes(
   `id_cancoes` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `cancao` VARCHAR(100) NOT NULL,
@@ -44,6 +86,27 @@ CREATE TABLE cancoes(
   FOREIGN KEY(`id_album`)
     REFERENCES `album` (`id_album`)
 ) ENGINE = InnoDB;
+
+INSERT INTO cancoes (cancao, id_album)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
 CREATE TABLE usuarios (
   `id_usuario` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
