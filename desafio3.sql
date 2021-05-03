486328f3d5acb782709fe45b202bf4ecd87f757b
MarceloBorges <melombm@hotmail.com>
2021-05-03 08:13:40 -0500

    codeclimate do requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS 
-	SELECT
+    SELECT
       us.nome_usuario AS usuario,
       cc.nome_cancao AS cancoes
-	FROM
-      SpotifyClone.usuarioTB AS us
-		      INNER JOIN
+    FROM
+      SpotifyClone.usuarioTB AS us INNER JOIN
       SpotifyClone.cancoesTB AS cc
 ORDER BY usuario, cancoes;   
 -- SELECT * FROM historico_reproducao_usuarios;
