cd7fb5afe38ad66aeea34fad81bebf381f89fe8f
Gustavo Fernandes Thirion <65987099+Gustaft86@users.noreply.github.com>
2021-05-03 11:02:53 -0300

    Delete desafio5.sql
    Codeclimate error

--- desafio5.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW top_2_hits_do_momento AS
-SELECT `historico_de_reproducoes` AS `cancao`, COUNT(*) AS `reproducoes`
-FROM SpotifyClone.Historicos
-GROUP BY `historico_de_reproducoes`
-ORDER BY `reproducoes` DESC, `cancao` ASC
-LIMIT 2;
