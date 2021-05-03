38a4400dd3f64fd147ce423eac2819fa019fc4b9
bidfca <bruninhakjurugat11@gmail.com>
2021-05-03 13:40:41 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nomeArtista VARCHAR(30))
+
+BEGIN
+SELECT artista.artista,
+album.album
+FROM SpotifyClone.artistas artista
+INNER JOIN SpotifyClone.albuns album
+ON album.artista_id = artista.artista_id
+WHERE artista.artista = nomeArtista
+END
+
+$$ DELIMITER ;
