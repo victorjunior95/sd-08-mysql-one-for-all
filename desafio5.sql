a284457674170302d8e0bd3874a7a613addfbc9d
MarceloBorges <melombm@hotmail.com>
2021-05-03 09:20:35 -0500

    requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+      cc.nome_cancao AS cancao, COUNT(*) AS reproducoes
+    FROM
+      SpotifyClone.cancoesTB AS cc INNER JOIN
+      SpotifyClone.historicoTB AS his ON his.cancaoID = cc.cancaoID
+GROUP BY cancao ORDER BY reproducoes DESC , cancao LIMIT 2;
+-- SELECT * FROM top_2_hits_do_momento;
+ 
