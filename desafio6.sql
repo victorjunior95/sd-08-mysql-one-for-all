3f5e4b2af4a8f81f09057a9d86682f455ab06ebe
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-05-03 17:44:00 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
-CREATE VIEW SpotifyClone.top_2_hits_do_momento AS
-SELECT M.musica_nome AS cancao, COUNT(S.musica_id) AS reproducoes
-FROM SpotifyClone.historico AS S
-INNER JOIN SpotifyClone.musicas AS M
-ON S.musica_id = M.musica_id
-GROUP BY S.musica_id
-ORDER BY reproducoes DESC, cancao
-LIMIT 2;
+CREATE VIEW SpotifyClone.faturamento_atual AS
+SELECT ROUND(MIN(P.plano_valor), 2) AS faturamento_minimo,
+ROUND(MAX(P.plano_valor), 2) AS faturamento_maximo,
+ROUND(AVG(P.plano_valor), 2) AS faturamento_medio,
+ROUND(SUM(P.plano_valor), 2) AS faturamento_total
+FROM SpotifyClone.usuarios AS U
+INNER JOIN SpotifyClone.planos AS P
+ON U.plano_id = P.plano_id;
