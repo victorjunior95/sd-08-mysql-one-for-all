06eeb694480d1ec372a7fc793eb679c4e136ac67
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-03 12:07:39 -0300

    Initial Commit - Clean Up

--- initialCommit.sh
+++ initialCommit.sh
@@ -1,6 +1,7 @@
-for i in {1...11}
-do
-    touch desafio$i.sql
+Lenght = 1
+while [$Lenght -lt 11]; do
+    touch desafio$Lenght.sql;
+    let Lenght = Lenght + 1;
 done
 touch desafio1.package.json
 git add --all
