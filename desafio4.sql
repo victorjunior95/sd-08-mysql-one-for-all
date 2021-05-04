2ad760e76bca3fed308fdc9fec46dcce1199bcd4
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-05-04 19:08:28 -0300

    iniciando o projeto MySQL One For All

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,14 @@
-CREATE VIEW top_3_artistas AS
+-- CREATE VIEW top_3_artistas AS
     SELECT 
-        (SELECT 
-                a.nome
-            FROM
-                SpotifyClone.artistas a
-            WHERE
-                a.id = s.artista_id) 'artista',
-        COUNT(s.usuario_id) 'seguidores'
-    FROM
-        SpotifyClone.seguidores s
-    GROUP BY `artista`
-    LIMIT 3;
+    (SELECT 
+            a.nome
+        FROM
+            SpotifyClone.artistas a
+        WHERE
+            a.id = s.artista_id) 'artista',
+    COUNT(s.usuario_id) 'seguidores'
+FROM
+    SpotifyClone.seguidores s
+GROUP BY `artista`
+ORDER BY `seguidores` DESC , `artista`
+LIMIT 3;
