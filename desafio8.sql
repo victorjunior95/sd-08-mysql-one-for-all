6ce1ccbb8cb04ba86d3584f34f385b51f5967e64
Uilton Rocha <itoclick@gmail.com>
2021-05-04 05:46:16 -0300

    req1 ao 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE
 ON  SpotifyClone._usuarios FOR EACH ROW
 BEGIN
-DELETE _historico FROM SpotifyClone._historico WHERE usuario_id = (SELECT usuario_id FROM DELETED);
-DELETE _seguindo FROM SpotifyClone._seguindo WHERE usuario_id = (SELECT usuario_id FROM DELETED);
+DELETE FROM SpotifyClone._historico WHERE usuario_id = (SELECT usuario_id FROM DELETED);
+DELETE FROM SpotifyClone._seguindo WHERE usuario_id = (SELECT usuario_id FROM DELETED);
 END $$
 DELIMITER ;
