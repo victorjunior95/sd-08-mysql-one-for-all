2a02a78b078ea77481148fe18446d99224edd327
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-05-04 13:46:49 -0300

    4 update

--- desafio1.sql
+++ desafio1.sql
@@ -55,10 +55,9 @@ INSERT INTO sons (nome, album_id)
 VALUES("Soul For Us", 1),("Reflections Of Magic", 1),("Dance With Her Own", 1),("Troubles Of My Inner Fire", 2),("Time Fireworks", 2),("Magic Circus", 3),("Honey, So Do I", 3),("Sweetie, Let's Go Wild", 3),("She Knows", 3),("Fantasy For Me", 4),("Celebration Of More", 4),("Rock His Everything", 4),("Home Forever", 4),("Diamond Power", 4),("Honey, Let's Be Silly", 4),("Thang Of Thunder", 5),("Words Of Her Life", 5),("Without My Streets", 5);
 
 CREATE TABLE seguidores (
-    id INT AUTO_INCREMENT,
     usuario_id INT,
     artista_id INT,
-    CONSTRAINT PRIMARY KEY (id , usuario_id , artista_id),
+    CONSTRAINT PRIMARY KEY (usuario_id , artista_id),
     FOREIGN KEY (usuario_id)
         REFERENCES usuario (id),
     FOREIGN KEY (artista_id)
@@ -69,7 +68,6 @@ INSERT INTO seguidores (usuario_id, artista_id)
 VALUES(1, 1),(1, 4),(1, 3),(2, 1),(2, 3),(3, 2),(3, 1),(4, 4);
 
 CREATE TABLE historico (
-    id INT AUTO_INCREMENT,
     usuario_id INT,
     sons_id INT,
     CONSTRAINT PRIMARY KEY (id , usuario_id , sons_id),
@@ -80,7 +78,7 @@ CREATE TABLE historico (
 )  ENGINE=INNODB;
 
 INSERT INTO historico (usuario_id, sons_id)
-VALUES1, 1),
+VALUES1 1),
 (1, 6),
 (1, 14),
 (1, 16),
