9862a9c575c68a4bf4e3ea017e6da34fa00e1d5e
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-05-04 12:18:31 -0400

    requisito 7 fix

--- desafio7.sql
+++ desafio7.sql
@@ -3,4 +3,4 @@ SELECT ar.nome AS `artista`, al.nome AS `album`, COUNT(usa.usuario_id) AS `segui
 INNER JOIN SpotifyClone.albuns AS al ON al.artista_id = ar.artista_id
 INNER JOIN SpotifyClone.usuario_seguindo_artista AS usa ON usa.artista_id = ar.artista_id
 GROUP BY al.album_id
-ORDER BY `seguidores` DESC, ar.nome;
+ORDER BY `seguidores` DESC, ar.nome, al.nome;
