f66b6175fb873948e872f013813c2e28cb1d2d19
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-05-04 14:11:59 -0300

    4 update

--- desafio1.sql
+++ desafio1.sql
@@ -1,49 +1,69 @@
+-- Colaboração:
+-- Arnaelcio: https://github.com/Arnaelcio
+-- Marcelo: https://github.com/Marcelllombm
 DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
-CREATE TABLE plano (
+CREATE TABLE planos (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
     valor DOUBLE
 )  ENGINE=INNODB;
 
-INSERT INTO plano (nome, valor)
-VALUES('gratuito', 0),('familiar', 7.99),('universitário', 5.99);
-
-CREATE TABLE usuario (
+INSERT INTO planos (nome, valor)
+VALUES
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
+    
+CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
     idade INT,
     plano_id INT,
     FOREIGN KEY (plano_id)
-        REFERENCES plano (id)
+        REFERENCES planos (id)
 )  ENGINE=INNODB;
 
-INSERT INTO usuario (nome, idade, plano_id)
-VALUES('Thati', 23, 1),('Cintia', 35, 2),('Bill', 20, 3),('Roger', 45, 1);
-CREATE TABLE artista (
+INSERT INTO usuarios (nome, idade, plano_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+CREATE TABLE artistas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50)
 )  ENGINE=INNODB;
     
-INSERT INTO artista (nome)
-VALUES('Walter Phoenix'),('Peter Strong'),('Lance Day'),('Freedie Shannon');
+INSERT INTO artistas (nome)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
 CREATE TABLE albuns (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
     artista_id INT,
     FOREIGN KEY (artista_id)
-        REFERENCES artista (id)
+        REFERENCES artistas (id)
 )  ENGINE=INNODB;
 
 INSERT INTO albuns (nome, artista_id)
-VALUES('Envious', 1),('Exuberant', 1),('Hallowed Steam', 2),('Incandescent', 3),('Temporary Culture', 4);
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
-CREATE TABLE sons (
+CREATE TABLE musicas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
     album_id INT,
@@ -51,41 +71,70 @@ CREATE TABLE sons (
         REFERENCES albuns (id)
 )  ENGINE=INNODB;
 
-INSERT INTO sons (nome, album_id)
-VALUES("Soul For Us", 1),("Reflections Of Magic", 1),("Dance With Her Own", 1),("Troubles Of My Inner Fire", 2),("Time Fireworks", 2),("Magic Circus", 3),("Honey, So Do I", 3),("Sweetie, Let's Go Wild", 3),("She Knows", 3),("Fantasy For Me", 4),("Celebration Of More", 4),("Rock His Everything", 4),("Home Forever", 4),("Diamond Power", 4),("Honey, Let's Be Silly", 4),("Thang Of Thunder", 5),("Words Of Her Life", 5),("Without My Streets", 5);
+INSERT INTO musicas (nome, album_id)
+VALUES
+("Soul For Us", 1),
+("Reflections Of Magic", 1),
+("Dance With Her Own", 1),
+("Troubles Of My Inner Fire", 2),
+("Time Fireworks", 2),
+("Magic Circus", 3),
+("Honey, So Do I", 3),
+("Sweetie, Let's Go Wild", 3),
+("She Knows", 3),
+("Fantasy For Me", 4),
+("Celebration Of More", 4),
+("Rock His Everything", 4),
+("Home Forever", 4),
+("Diamond Power", 4),
+("Honey, Let's Be Silly", 4),
+("Thang Of Thunder", 5),
+("Words Of Her Life", 5),
+("Without My Streets", 5);
 
 CREATE TABLE seguidores (
+    id INT AUTO_INCREMENT,
     usuario_id INT,
     artista_id INT,
-    CONSTRAINT PRIMARY KEY (usuario_id , artista_id),
+    CONSTRAINT PRIMARY KEY (id , usuario_id , artista_id),
     FOREIGN KEY (usuario_id)
-        REFERENCES usuario (id),
+        REFERENCES usuarios (id),
     FOREIGN KEY (artista_id)
-        REFERENCES artista (id)
+        REFERENCES artistas (id)
 )  ENGINE=INNODB;	
 
 INSERT INTO seguidores (usuario_id, artista_id)
-VALUES(1, 1),(1, 4),(1, 3),(2, 1),(2, 3),(3, 2),(3, 1),(4, 4);
+VALUES
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
 
 CREATE TABLE historico (
+    id INT AUTO_INCREMENT,
     usuario_id INT,
-    sons_id INT,
-    CONSTRAINT PRIMARY KEY (id , usuario_id , sons_id),
+    musica_id INT,
+    CONSTRAINT PRIMARY KEY (id , usuario_id , musica_id),
     FOREIGN KEY (usuario_id)
-        REFERENCES usuario (id),
-    FOREIGN KEY (sons_id)
-        REFERENCES sons (id)
+        REFERENCES usuarios (id),
+    FOREIGN KEY (musica_id)
+        REFERENCES musicas (id)
 )  ENGINE=INNODB;
 
-INSERT INTO historico (usuario_id, sons_id)
-VALUES1 1),
+INSERT INTO historico (usuario_id, musica_id)
+VALUES
+(1, 1),
 (1, 6),
 (1, 14),
 (1, 16),
 (2, 13),
 (2, 17),
 (2, 2),
-(2, 7),
+(2, 15),
 (3, 4),
 (3, 16),
 (3, 6),
