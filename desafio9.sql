9a5464c9164c6c962e5ab4616c282d9d98c4171b
Uilton Rocha <itoclick@gmail.com>
2021-05-04 06:00:25 -0300

    req1 ao 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,10 @@
-.
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN param_nome_artista VARCHAR(50))
+BEGIN
+  SELECT a.artista AS artista, ab.album AS album
+  FROM SpotifyClone._artistas AS a
+  INNER JOIN SpotifyClone._albums AS ab ON a.artista_id = ab.artista_id
+  WHERE `artista` = `param_nome_artista`
+  ORDER BY `album`;
+END $$ 
+DELIMITER ;
