27aec62c880b46bc9b37e7c17accf4beb413eae8
Uilton Rocha <itoclick@gmail.com>
2021-05-04 22:14:54 -0300

    req 7 e 9 debug ainda não passa

--- desafio9.sql
+++ desafio9.sql
@@ -4,7 +4,7 @@ BEGIN
   SELECT a.artista AS artista, ab.album AS album
   FROM SpotifyClone._artistas AS a
   INNER JOIN SpotifyClone._albums AS ab ON a.artista_id = ab.artista_id
-  WHERE `artista` = `param_nome_artista`
-  ORDER BY `album`;
+  WHERE artista = param_nome_artista
+  ORDER BY album;
 END $$ 
 DELIMITER ;
