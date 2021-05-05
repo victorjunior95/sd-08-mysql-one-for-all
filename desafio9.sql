b5df6e546c3e07e3110769cb98782dcd5d640e0c
Uilton Rocha <itoclick@gmail.com>
2021-05-04 22:25:25 -0300

    req 7 e 9 debug ainda não passa

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,12 @@
+-- Verificar bug
+USE SpotifyClone;
 DELIMITER $$
-CREATE PROCEDURE albuns_do_artista(IN param_nome_artista VARCHAR(50))
+CREATE PROCEDURE albuns_do_artista(IN art VARCHAR(80))
 BEGIN
-  SELECT a.artista AS artista, ab.album AS album
-  FROM SpotifyClone._artistas AS a
+  SELECT a.artista AS 'artista', ab.album AS 'album'
+  FROM _artistas AS a
   INNER JOIN SpotifyClone._albums AS ab ON a.artista_id = ab.artista_id
-  WHERE artista = param_nome_artista
+  WHERE artista = art
   ORDER BY album;
 END $$ 
 DELIMITER ;
