2d641051d2386b59ee0d557965ef7ae0a54d3b36
Uilton Rocha <itoclick@gmail.com>
2021-05-05 19:16:04 -0300

    req 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,6 @@ CREATE VIEW cancoes_premium AS
 SELECT m.cancoes AS nome, count(h.cancoes_id) AS reproducoes
 FROM SpotifyClone._musicas AS m
 inner join SpotifyClone._historico AS h on m.cancoes_id = h.cancoes_id
+inner join SpotifyClone._usuarios as u on u.usuario_id = h.usuario_id
+WHERE u.plano_id <> 1
 GROUP BY nome ORDER BY nome;
