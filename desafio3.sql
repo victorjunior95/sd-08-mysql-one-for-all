9bd6ea9141f0e289e0c2661393e70bd6481d715a
Afonsofn <afonsoafn@gmail.com>
2021-05-05 08:26:36 -0300

    requisito 3 4 5

---
+++ desafio3.sql
@@ -0,0 +1,1 @@
+CREATE VIEW historico_reproducao_usuarios AS SELECT u.usuario_nome AS `usuario`, c.cancao_nome AS `nome` FROM SpotifyClone.usuarios AS u INNER JOIN SpotifyClone.usuario_cancao AS uc ON u.usuario_id = uc.usuario_id INNER JOIN SpotifyClone.cancoes AS c ON uc.cancao_id = c.cancao_id ORDER BY usuario , nome;
---
+++ desafio4.sql
@@ -0,0 +1,1 @@
+CREATE VIEW top_3_artistas AS SELECT a.artista_nome AS artista, COUNT(ua.usuario_id) AS seguidores FROM usuario_artista AS ua INNER JOIN artistas AS a ON ua.artista_id = a.artista_id GROUP BY ua.artista_id ORDER BY seguidores DESC, artista ASC LIMIT 3;
