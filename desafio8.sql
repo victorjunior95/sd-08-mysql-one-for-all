fb6d0113817eb25e80ed3485841b350a846c7762
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-05 17:04:38 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.seguindo WHERE id_usuario = OLD.id_usuario;
+DELETE FROM SpotifyClone.historico_cancoes WHERE id_usuario = OLD.id_usuario;
+END $$
+DELIMITER  ;
