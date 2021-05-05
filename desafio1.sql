1aafdce63a6af1c4bef32d737bb36ae283a0384f
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-05 11:22:20 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -5,21 +5,111 @@ USE SpotifyClone;
 
 CREATE TABLE planos (
   plano_id INT PRIMARY KEY AUTO_INCREMENT,
-  plano VARCHAR(50) NOT NULL,
+  plano VARCHAR(100) NOT NULL,
   valor_plano DOUBLE NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE usuarios (
   ususario_id INT PRIMARY KEY AUTO_INCREMENT,
-  
+  usuario VARCHAR(100) NOT NULL,
+  idade INT NOT NULL,
+  plano_id INT NOT NULL,
+  FOREIGN KEY (plano_id) REFERENCES planos (plano_id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE artistas () ENGINE = InnoDB;
+CREATE TABLE artistas (
+  artista_id INT PRIMARY KEY AUTO_INCREMENT,
+  artista VARCHAR(100)
+) ENGINE = InnoDB;
 
-CREATE TABLE albuns () ENGINE = InnoDB;
+CREATE TABLE albuns (
+  album_id INT PRIMARY KEY AUTO_INCREMENT,
+  album VARCHAR(100),
+  artista_id INT NOT NULL,
+  FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
+) ENGINE = InnoDB;
 
-CREATE TABLE musicas () ENGINE = InnoDB;
+CREATE TABLE musicas (
+  musica_id INT PRIMARY KEY AUTO_INCREMENT,
+  musica VARCHAR(100) NOT NULL,
+  artista_id INT NOT NULL,
+  album_id INT NOT NULL,
+  FOREIGN KEY (artista_id) REFERENCES artistas (artista_id),
+  FOREIGN KEY (album_id) REFERENCES albuns (album_id)
+) ENGINE = InnoDB;
 
-CREATE TABLE  seguindo () ENGINE = InnoDB;
+CREATE TABLE  seguindo (
+  usuario_id INT NOT NULL,
+  artista_id INT NOT NULL,
+  PRIMARY KEY (usuario_id, artista_id),
+  FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id),
+  FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
+) ENGINE = InnoDB;
 
-CREATE TABLE historico_reproducao () ENGINE = InnoDB;
+CREATE TABLE historico_reproducao (
+  usuario_id INT NOT NULL,
+  musica_id INT NOT NULL,
+  PRIMARY KEY (usuario_id, musica_id),
+  FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id),
+  FOREIGN KEY (musica_id) REFERENCES musicas (musica_id)
+) ENGINE = InnoDB;
+
+INSERT INTO usuarios (usuario, idade, plano_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO planos (plano, valor_plano)
+VALUES
+('gratuito', '0.00'),
+('familiar', '7.99'),
+('universitário', '5.99');
+
+INSERT INTO artistas (artista)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO albuns (album, artista_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO musicas (musica, album_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO seguindo (usuario_id, artista_id)
+VALUES
+(1, 1),
+(1, 3),
+(1, 4),
+(2,	1),
+(2,	3),
+(3,	1),
+(3,	2),
+(4,	4);
