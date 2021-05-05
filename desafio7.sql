885c9432069bbc2536f28bbff0ea9dcd3b3b2aec
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-05 15:41:50 -0300

    desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 CREATE VIEW SpotifyClone.perfil_artistas AS
 SELECT art.artista, alb.album, COUNT(se.id_artista) AS 'seguidores'
-FROM artistas AS ar
-INNER JOIN albuns AS al ON art.id_artista = alb.id_artista
+FROM artistas AS art
+INNER JOIN albuns AS alb ON art.id_artista = alb.id_artista
 INNER JOIN seguindo AS se ON art.id_artista = se.id_artista
 GROUP BY alb.id_album
 ORDER BY `seguidores` DESC , art.artista , alb.album;
