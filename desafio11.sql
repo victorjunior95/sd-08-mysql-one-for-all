bb313a3aa2e4d2876778bdb876046f7cd364da5e
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-05 01:10:56 -0300

    req11 reproducoes

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 create view cancoes_premium as 
-select m.musica as nome, count(hr.musica_id) from SpotifyClone.musicas m
+select m.musica as nome, count(hr.musica_id) as reproducoes from SpotifyClone.musicas m
 inner join SpotifyClone.historico_reproducoes hr on m.musica_id = hr.musica_id
 inner join SpotifyClone.usuarios u on u.usuario_id = hr.usuario_id
 where u.plano_id in (2, 3) group by m.musica
