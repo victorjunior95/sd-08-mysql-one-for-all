1ac3b383f95791b05a2487114c4dbc08157bd089
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-05 15:07:35 -0300

    REQ-4 BUILD

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT A.artist_name AS "artista",
+COUNT(F.user_id) AS "seguidores"
+FROM SpotifyClone.follower AS F
+INNER JOIN SpotifyClone.artists AS A ON F.artist_id = A.artist_id
+GROUP BY A.artist_id
+ORDER BY seguidores DESC, artista
+LIMIT 3;
