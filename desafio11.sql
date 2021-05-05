a2c956ad9f9a95ac77ad5bb57d81ba77e51af412
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-05 18:20:46 -0300

    [FIX]: REQ-11 BUILD

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 CREATE VIEW cancoes_premium AS
 SELECT
 S.song_name AS "nome",
-COUNT(H.user_id) AS "reproducoes"
-FROM
-SpotifyClone.songs AS S
-INNER JOIN SpotifyClone.historic AS A ON S.song_id = H.song_id
-INNER JOIN SpotifyClone.users AS U ON U.user_id = H.user_id
-WHERE U.plan_id IN (2, 3)
+COUNT(H.song_id) AS "reproducoes"
+FROM historic AS H
+INNER JOIN songs AS S ON S.song_id = H.song_id
+INNER JOIN users AS U ON U.user_id = H.user_id
+INNER JOIN plans AS P ON P.plan_id = U.plan_id
+WHERE P.plan_id IN (2, 3)
 GROUP BY S.song_name
-ORDER BY S.song_name;
+ORDER BY 1;
