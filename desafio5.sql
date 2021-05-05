214917cae43e88ab7815bc3fbc19bcd960a03462
Bruno Wesley <bruno.wesley77@gmail.com>
2021-05-05 18:14:41 -0300

    refactor

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT m.music_name AS 'cancao', COUNT(*) AS 'reproducoes' 
+SELECT m.music_name AS cancao, COUNT(*) AS reproducoes 
 FROM SpotifyClone.reproduction_history h
 INNER JOIN SpotifyClone.music m ON h.music_id = m.music_id
 GROUP BY h.music_id
-ORDER BY `reproducoes` DESC, `cancao`
+ORDER BY reproducoes DESC, cancao
 LIMIT 2;
