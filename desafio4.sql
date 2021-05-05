b6218dda41f21b4ebab89130fffef21dc34d2c4a
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-05 14:01:40 -0300

    desafio 3-4

--- desafio4.sql
+++ desafio4.sql
@@ -1,1 +1,6 @@
-
+CREATE VIEW SpotifyClone.top_3_artistas AS
+SELECT a.artista, COUNT(s.id_artista) AS seguidores
+FROM artistas a
+INNER JOIN seguindo s ON a.id_artista = s.id_artista
+GROUP BY a.artista
+ORDER BY seguidores DESC, a.artista LIMIT 3;
