2674de5639d4e57eea479f0d16ebb83745f5ed2e
Bruno Wesley <bruno.wesley77@gmail.com>
2021-05-05 21:47:47 -0300

    refactor

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON user FOR EACH ROW
 BEGIN
 DELETE FROM followed_artist WHERE user_id = OLD.user_id;
-DELETE FROM playlist_history 
+DELETE FROM reproduction_history 
 WHERE user_id = OLD.user_id;
 END $$
 
