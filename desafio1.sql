657a040f574d0ce2e081289da27f139326c49888
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-07 18:45:06 -0300

    Inserçao dos dados na tabela e inserçao dos itens no desafio1.json

--- desafio1.sql
+++ desafio1.sql
@@ -59,3 +59,80 @@ CREATE TABLE Seguindo (
     FOREIGN KEY (artista_id)
         REFERENCES Artista (artista_id)
 )  ENGINE = INNODB;
+
+INSERT INTO Plano(plano, valores)
+VALUES
+("gratuito", 0.00),
+("universitario", 5.99),
+("familiar", 7.99);
+
+INSERT INTO Usuario(usuario, idade, plano_id)
+VALUES
+("Thati", 23, 1),
+("Cintia", 35, 3),
+("Bill", 20, 2),
+("Roger", 45, 1);
+
+INSERT INTO Artista(artista)
+VALUES
+("Walter Phoenix"),
+("Peter Strong"),
+("Lance Day"),
+("Freedie Shannon");
+
+INSERT INTO Album(album, artista_id)
+VALUES
+("Envious", 1),
+("Exuberant", 1),
+("Hallowed Steam", 2),
+("Incandescent", 3),
+("Temporary Culture", 4);
+
+INSERT INTO Musicas(musica, album_id)
+VALUES
+("Soul For Us", 1),
+("Reflections Of Magic", 1),
+("Dance With Her Own", 1),
+("Troubles Of My Inner Fire", 1),
+("Time Fireworks", 1),
+("Magic Circus", 2),
+("Honey, So Do I", 2),
+("Sweetie, Let's Go Wild", 2),
+("She Knows", 2),
+("Fantasy For Me", 3),
+("Celebration Of More", 3),
+("Rock His Everything", 3),
+("Home Forever", 3),
+("Diamond Power", 3),
+("Honey, Let's Be Silly", 3),
+("Thang Of Thunder", 4),
+("Words Of Her Life", 4),
+("Without My Streets", 4);
+
+INSERT INTO Seguindo(artista_id, usuario_id)
+VALUES
+(1, 1),
+(4, 1),
+(3, 1),
+(1, 2),
+(3, 2),
+(2, 3),
+(1, 3),
+(4, 4);
+
+INSERT INTO Historico(usuario_id, musica_id)
+VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
