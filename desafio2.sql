f3028eb806da0b08e19f5b05bdb4a42d1e7ced75
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-07 21:43:37 -0300

    Requisito 2 - correçao

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
-CREATE VIEW estatisticas_musicais AS 
+CREATE VIEW estatisticas_musicais AS
 SELECT
 COUNT(*) AS cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.Artista) AS artistas,
-(SELECT COUNT(*) FROM SpotifyClone.Album) AS albuns,
+(SELECT COUNT(*) FROM SpotifyClone.Album) AS albuns 
 FROM SpotifyClone.Musicas;
