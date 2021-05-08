cd3d27f48173d14bd208b3ca2d8c3da16841d6c8
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-08 11:52:23 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,12 @@
 CREATE VIEW cancoes_premium AS
 SELECT
-
-FROM
+musica.musica AS 'nome',
+COUNT(historico.musica_id) AS 'reproducoes'
+FROM SpotifyClone.Musicas AS musica
+INNER JOIN SpotifyClone.Historico AS historico
+ON historico.musica_id = musica.musica_id
+INNER JOIN SpotifyClone.Usuario AS user
+ON user.usuario_id = historico.usuario_id
+WHERE user.plano_id IN (2, 3)
+GROUP BY musica.musica
+ORDER BY 1;
