6accc5930c93e8efd2f45a0a1b85c02d7466b454
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-08 10:52:53 -0300

    Requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,7 +2,13 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(100))
 BEGIN
-  SELECT 
+  SELECT
+  art.artista AS 'artista',
+  album.album AS 'album'
+  FROM SpotifyClone.Artista AS art
+  INNER JOIN SpotifyClone.Album AS album
+  ON art.artista_id = album.artista_id
+  WHERE nome_artista = art.artista;
 END $$
 
 DELIMITER ;
