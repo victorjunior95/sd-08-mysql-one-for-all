3fe01be513746c6e771922515193dd4ecc19bffa
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-08 09:23:01 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+MIN(plano.valores) AS 'faturamento_minimo',
+MAX(plano.valores) AS 'faturamento_maximo',
+ROUND(AVG(plano.valores)) AS 'faturamento_medio',
+SUM(plano.valores) AS 'faturamento_total'
+FROM SpotifyClone.Plano AS plano
+INNER JOIN SpotifyClone.Usuario AS usuario
+ON usuario.plano_id = plano.plano_id;
