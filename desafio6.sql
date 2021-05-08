e161ee913841dfbd1744905fea390c02cfc26729
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-08 09:28:51 -0300

    Requisito 6 correcao do formato da media

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT
 MIN(plano.valores) AS 'faturamento_minimo',
 MAX(plano.valores) AS 'faturamento_maximo',
-ROUND(AVG(plano.valores)) AS 'faturamento_medio',
+ROUND(AVG(plano.valores), 2) AS 'faturamento_medio',
 SUM(plano.valores) AS 'faturamento_total'
 FROM SpotifyClone.Plano AS plano
 INNER JOIN SpotifyClone.Usuario AS usuario
