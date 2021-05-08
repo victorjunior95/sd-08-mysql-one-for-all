dff985491e923a737757262f543aafb7fd3e4ee8
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-07 21:59:01 -0300

    Requisito 3 - reestruturando

--- desafio3.sql
+++ desafio3.sql
@@ -3,8 +3,8 @@ SELECT
 user.usuario AS 'usuario',
 cancoes.musica AS 'nome'
 FROM Usuario AS user
-INNER JOIN Historico AS historico
+INNER JOIN SpotifyClone.Historico AS historico
 ON user.usuario_id = historico.usuario_id
-INNER JOIN cancoes AS cancoes
+INNER JOIN SpotifyClone.Musicas AS cancoes
 ON cancoes.musica_id = historico.musica_id
 ORDER BY 1, 2;
