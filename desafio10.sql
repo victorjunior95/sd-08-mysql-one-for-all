604223b783e55ad32a27af30bd2c5e8e22507a66
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-08 11:27:37 -0300

    Requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,16 @@
 DELIMITER $$
 
-CREATE FUNCTION
+CREATE FUNCTION quantidade_musicas_no_historico (idHistorico INT)
 RETURNS INT READS SQL DATA
 BEGIN
-  SELECT
+  DECLARE quantidade INT;
+    SELECT COUNT(*)
+    FROM SpotifyClone.Historico AS historico
+    WHERE idHistorico = historico.usuario_id
+    INTO quantidade;
+  RETURN quantidade;
 END $$
 
 DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
