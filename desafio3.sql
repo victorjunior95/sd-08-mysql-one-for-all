721c0329026b42d2cc3623b2859d8f6720b6e06b
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-08 08:44:38 -0300

    Requisito 4 refazendo a logica e padronizando o requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -2,9 +2,9 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT
 user.usuario AS 'usuario',
 cancoes.musica AS 'nome'
-FROM Usuario AS user
-INNER JOIN SpotifyClone.Historico AS 'historico'
+FROM SpotifyClone.Usuario AS user
+INNER JOIN SpotifyClone.Historico AS historico
 ON user.usuario_id = historico.usuario_id
-INNER JOIN SpotifyClone.Musicas AS 'cancoes'
+INNER JOIN SpotifyClone.Musicas AS cancoes
 ON cancoes.musica_id = historico.musica_id
 ORDER BY 1, 2;
--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 CREATE VIEW top_3_artistas AS 
 SELECT
-art.artista_nome AS 'artista',
+art.artista AS 'artista',
 COUNT(*) AS 'seguidores'
-FROM SpotifyClone.Artista AS 'art'
-INNER JOIN SpotifyClone.Seguindo as 'seguindo'
+FROM SpotifyClone.Artista AS art
+INNER JOIN SpotifyClone.Seguindo as seguindo
 ON seguindo.artista_id = art.artista_id
-GROUP BY art.artista_nome
+GROUP BY art.artista
 ORDER BY 2 DESC, 1
 LIMIT 3;
