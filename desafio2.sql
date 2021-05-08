a0c27a0fcefa4fef29731c50a7fb3e04a375082f
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-05-07 21:13:11 -0300

    Requisito 2 - correçao da virgula

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicas AS 
 SELECT
-COUNT(*) AS cancoes;
+COUNT(*) AS cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.Artista) AS artistas,
 (SELECT COUNT(*) FROM SpotifyClone.Album) AS albuns,
 FROM SpotifyClone.Musicas;
