f2b6f2a9a13b3b272a3c5bf76c0a8ab880c25bbe
adrianWarrior <adrianoforcellini>
2021-05-09 17:37:45 -0300

    correçao desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,8 @@
 -- consultei o repositorio de arnaelcio
 -- https://github.com/tryber/sd-08-mysql-one-for-all/blob/65480a31863f87fb24c5dedd1c98659c17420ecc/desafio1.sql
 -- https://www.w3schools.com/sql/sql_view.asp
--- "FAZ UMA TABELA"CREATE VIEW estatisticas_musicais AS
+-- "FAZ UMA TABELA"
+CREATE VIEW estatisticas_musicais AS
     SELECT 
         (SELECT 
                 COUNT(*)
