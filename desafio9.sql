4609053afc90c0a9e291755efe30ddf34b21cc06
adrianWarrior <adrianoforcellini>
2021-05-13 20:03:28 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+DELIMITER $$ 
+CREATE PROCEDURE albuns_do_artista(IN buscaNome VARCHAR(30))
+  BEGIN
+    SELECT 
+    artista.nome 'artista', 
+    albuns.nome 'album'
+    FROM artistas
+    INNER JOIN albuns ON albuns.artista_id = artista.id
+    WHERE artista.nome = buscaNome
+    ORDER BY albuns.nome ;
+  END
+$$ 
+DELIMITER ;
