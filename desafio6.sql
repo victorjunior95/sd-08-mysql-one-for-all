f43a56b437c9704649f04e9fdca442f4e967221e
adrianWarrior <adrianoforcellini>
2021-05-13 18:50:32 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS
+SELECT 
+    ROUND(MIN(planos.valor), 2) faturamento_minimo,
+        ROUND(MAX(planos.valor), 2) faturamento_maximo,
+        ROUND(SUM(planos.valor) / 4, 2) faturamento_medio,
+        ROUND(SUM(planos.valor), 2) faturamento_total
+FROM
+    SpotifyClone.usuarios
+        INNER JOIN
+    SpotifyClone.planos ON usuarios.plano_id = planos.id
