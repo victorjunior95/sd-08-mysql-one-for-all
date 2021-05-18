468a424a020cd7c8f77673fc15529efcdb83de03
Victor Moraes <moraesvictorgk@gmail.com>
2021-05-18 11:35:45 -0300

    troca usuário para usuario

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT user_name AS usuário,
+SELECT user_name AS usuario,
 song_name AS nome
 FROM SpotifyClone.user AS U
 INNER JOIN SpotifyClone.my_playlist AS MP
 ON U.user_id = MP.user_id
 INNER JOIN SpotifyClone.song AS S
 ON MP.song_id = S.song_id
-ORDER BY usuário, nome;
+ORDER BY usuario, nome;
