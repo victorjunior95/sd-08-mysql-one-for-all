0414d47526b74095cd3d13f08ac07317fafe2a1d
Victor Moraes <moraesvictorgk@gmail.com>
2021-05-19 08:14:15 -0300

    rewrite desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,15 +1,15 @@
 USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico (name INT)
+CREATE FUNCTION quantidade_musicas_no_historico(userId INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE my_playlist_count INT;
-SELECT COUNT(*) AS quantidade_musicas_no_historico 
-FROM SpotifyClone.my_playlist AS MP
-INNER JOIN SpotifyClone.user U
-ON MP.user_id = U.user_id
-WHERE user_id = name INTO my_playlist_count;
+SELECT COUNT(*) FROM SpotifyClone.user AS U
+INNER JOIN SpotifyClone.my_playlist AS MP 
+ON U.user_id = MP.user_id
+WHERE U.user_id = userId
+INTO my_playlist_count;
 RETURN my_playlist_count;
 END $$
 
