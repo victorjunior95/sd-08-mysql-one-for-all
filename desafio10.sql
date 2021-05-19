a29c59d0730c05abf33af7d031ce967022c2e361
Victor Moraes <moraesvictorgk@gmail.com>
2021-05-18 22:57:58 -0300

    desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -9,9 +9,7 @@ SELECT COUNT(*) AS quantidade_musicas_no_historico
 FROM SpotifyClone.my_playlist AS MP
 INNER JOIN SpotifyClone.user U
 ON MP.user_id = U.user_id
-GROUP BY user_name
-HAVING
-    user_name = name INTO my_playlist_count;
+WHERE user_id = name INTO my_playlist_count;
 RETURN my_playlist_count;
 END $$
 
