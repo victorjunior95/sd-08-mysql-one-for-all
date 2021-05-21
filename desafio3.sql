c9e14c106346fe324694b75ed295a88d6ac41958
Rafael <rafaeljavaro750@gmail.com>
2021-05-20 23:12:00 -0300

    CodeClimate Desafio 2 e 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-	(SELECT user_name FROM SpotifyClone.users as u
-		WHERE  u.user_id = uh.user_id) AS usuario,
-	(SELECT song_name FROM SpotifyClone.songs AS s
-		WHERE s.song_id = uh.song_id) AS nome
+(SELECT user_name FROM SpotifyClone.users as u
+	WHERE  u.user_id = uh.user_id) AS usuario,
+(SELECT song_name FROM SpotifyClone.songs AS s
+	WHERE s.song_id = uh.song_id) AS nome
 FROM SpotifyClone.user_history AS uh
 ORDER BY usuario, nome;
