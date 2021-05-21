74aea18caa23cfdc60a2ef38a800e04caddd1773
enicio <jessandro42@gmail.com>
2021-05-21 12:47:00 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER  trigger_usuario_delete
+BEFORE DELETE ON usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
