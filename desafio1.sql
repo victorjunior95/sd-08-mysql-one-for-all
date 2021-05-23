f3e5358decab234fa9e502683f7bf9c0af8f1559
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-23 20:37:16 -0300

    REQ 01 - fix codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -5,52 +5,52 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE plans(
-    plan_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    value DECIMAL(5, 2) NOT NULL
+  plan_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  value DECIMAL(5, 2) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE users(
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    plan_id INT NOT NULL DEFAULT 0,
-    FOREIGN KEY (plan_id) REFERENCES plans (plan_id)
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  age INT NOT NULL,
+  plan_id INT NOT NULL DEFAULT 0,
+  FOREIGN KEY (plan_id) REFERENCES plans (plan_id)
 ) engine = InnoDB;
 
 CREATE TABLE artists(
-    artist_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL
+  artist_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE followers(
-	user_id INT NOT NULL,
-    artist_id INT NOT NULL,
-    PRIMARY KEY (user_id, artist_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+  user_id INT NOT NULL,
+  artist_id INT NOT NULL,
+  PRIMARY KEY (user_id, artist_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE albums(
-    album_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    artist_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+  album_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  artist_id INT NOT NULL,
+  FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE songs(
-    song_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    album_id INT NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES albums (album_id)
+  song_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  album_id INT NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES albums (album_id)
 ) engine = InnoDB;
 
 CREATE TABLE histories(
-	user_id INT NOT NULL,
-    song_id INT NOT NULL,
-    PRIMARY KEY (user_id, song_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (song_id) REFERENCES songs (song_id)
+  user_id INT NOT NULL,
+  song_id INT NOT NULL,
+  PRIMARY KEY (user_id, song_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (song_id) REFERENCES songs (song_id)
 ) engine = InnoDB;
 
 INSERT INTO plans (name, value)
