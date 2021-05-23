a3fd82d6106f867a0fa9bd2be6ef1bcadedb311e
Elisa <elisatcamillo@gmail.com>
2021-05-23 18:20:14 -0300

    codeclimate

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT
-	COUNT(DISTINCT songs.song_id) AS cancoes, 
-	COUNT(DISTINCT artists.artist_id) AS artistas, 
-	COUNT(DISTINCT albums.album_id) AS albuns
-FROM 
-	SpotifyClone.songs AS songs 
-	CROSS JOIN SpotifyClone.artists AS artists 
-	CROSS JOIN SpotifyClone.albums AS albums;
--- https://www.w3schools.com/mysql/mysql_join_cross.asp
+  SELECT
+    COUNT(DISTINCT songs.song_id) AS cancoes, 
+    COUNT(DISTINCT artists.artist_id) AS artistas, 
+    COUNT(DISTINCT albums.album_id) AS albuns
+  FROM 
+    SpotifyClone.songs AS songs 
+    CROSS JOIN SpotifyClone.artists AS artists 
+    CROSS JOIN SpotifyClone.albums AS albums;
+  -- https://www.w3schools.com/mysql/mysql_join_cross.asp
