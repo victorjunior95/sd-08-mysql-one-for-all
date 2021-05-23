54942f7d8eeb1d72490f15d3eea253519ddea021
Elisa <elisatcamillo@gmail.com>
2021-05-23 17:11:10 -0300

    2

--- desafio2.sql
+++ desafio2.sql
@@ -10,9 +10,9 @@ DROP VIEW IF EXISTS estatisticas_musicais;
 
 CREATE VIEW estatisticas_musicais AS
   SELECT 
-    COUNT(DISTINCT songs.song_id) AS cancoes, 
-    COUNT(DISTINCT artists.artist_id) AS artistas, 
-    COUNT(DISTINCT albums.album_id) AS albums
+    COUNT(DISTINCT songs.song_id) AS cancoes,
+    COUNT(DISTINCT artists.artist_id) AS artistas,
+    COUNT(DISTINCT albums.album_id) AS albuns
   FROM 
     SpotifyClone.songs AS songs
     CROSS JOIN SpotifyClone.artists AS artists
