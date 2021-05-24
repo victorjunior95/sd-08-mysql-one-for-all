bcfd8d2e7bd21450b897a5586aa2b9c08b25c22d
Isabella Paz <bellapaz.iorp@gmail.com>
2021-05-24 13:02:35 -0300

    desafio 5 versao 2

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_2_hits_do_momento AS
   SELECT
-    C.cancao AS cancao
+    C.cancao AS cancao,
     COUNT(*) AS reproducoes
   FROM SpotifyClone.cancoes AS C
   INNER JOIN SpotifyClone.historicos_de_reproducoes AS HR
