23c80e78199b55f2e6edca7ec36bce108d78a8a1
IzeldaValeriano <zel.valeriano@gmail.com>
2021-05-24 11:58:36 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,8 @@
-DELIMITER $$
+DELIMITER $M
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
   FOR EACH ROW
 BEGIN
   DELETE FROM artistas_seguindo WHERE usuario_id = OLD.usuario_id;
   DELETE FROM historico_reproducoes WHERE usuario_id = OLD.usuario_id;
-END $$ DELIMITER ;
+END $M DELIMITER ;
