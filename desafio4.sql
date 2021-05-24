d742a06620849ac7139d28db20662056c494599c
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-23 21:15:45 -0300

    REQ 04

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_3_artistas AS
+  SELECT
+    artists.name AS artista,
+    COUNT(followers.artist_id) AS seguidores
+  FROM SpotifyClone.followers
+  JOIN SpotifyClone.artists ON artists.artist_id = followers.artist_id
+  GROUP BY followers.artist_id
+  ORDER BY seguidores DESC
+  LIMIT 3;
