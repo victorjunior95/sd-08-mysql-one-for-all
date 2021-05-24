b4a6e24c2496622c09a6430e334fb259ec59c782
Isabella Paz <bellapaz.iorp@gmail.com>
2021-05-24 09:52:50 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+  SELECT
+    A.artista AS artista,
+    COUNT(artista_id) AS seguidores
+  FROM SpotifyClone.seguidores AS S
+  INNER JOIN SpotifyClone.artistas AS A
+    ON A.artista_id = S.artista_id
+  GROUP BY artista
+  ORDER BY seguidores DESC, artista;
+  LIMIT 3;
