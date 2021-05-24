5d3b4e058915c31200a973be3bbabdd293a48fb6
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-23 21:33:22 -0300

    REQ 04 - fix order

--- desafio4.sql
+++ desafio4.sql
@@ -5,5 +5,5 @@ CREATE VIEW top_3_artistas AS
   FROM SpotifyClone.followers
   JOIN SpotifyClone.artists ON artists.artist_id = followers.artist_id
   GROUP BY followers.artist_id
-  ORDER BY seguidores DESC
+  ORDER BY seguidores DESC, artista
   LIMIT 3;
