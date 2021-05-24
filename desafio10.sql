b0853bd9aecfca2f4a9dfd44156ceb13548ea5cd
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-23 22:36:49 -0300

    REQ 10 - change parameter from varchar to int

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(user VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(user INT)
   RETURNS INT READS SQL DATA
   BEGIN
     DECLARE number_of_songs_played INT;
@@ -9,7 +9,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(user VARCHAR(100))
       COUNT(*)
     FROM histories
     JOIN users ON users.user_id = histories.user_id
-    WHERE users.name = user
+    WHERE users.user_id = user
     INTO number_of_songs_played;
     RETURN number_of_songs_played;
   END $$
