9a06ae6fa9b6e51bdb814455202dc01eec026dff
Isabella Paz <bellapaz.iorp@gmail.com>
2021-05-24 09:53:37 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW faturamento_atual AS
+  SELECT
+    ROUND(MIN(P.preco), 2) AS faturamento_minimo,
+    ROUND(MAX(P.preco), 2) AS faturamento_maximo,
+    ROUND(AVG(P.preco, 2) AS faturamento_medio,
+    ROUND(SUM(P.preco), 2) AS faturamento_total
+  FROM SpotifyClone.planos AS P
+  INNER JOIN SpotifyClone.usuarios AS U
+    ON P.plano_id = U.plan_id;
